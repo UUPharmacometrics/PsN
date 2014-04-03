6fc7beeff81189254878e20680d0b7f00c74ce83
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2014-04-03 10:41:04 +0200

    Webpage: updated installation instructions for PsN4

--- development/webpage/htdocs/install.php
+++ development/webpage/htdocs/install.php
@@ -5,28 +5,30 @@
 Before you install PsN, you should make sure that the following programs/modules are installed on your computer. 
 
 <ul>
-<li>NONMEM. <p>
+<li>NONMEM<p>
 Please note that with NMQual8 the nm72.xml file must be slightly modified (to expect .mod instead of .ctl as the control stream suffix) before NONMEM installation, see document psn_configuration.pdf, otherwise PsN's NMQual8 support will not work.<p>
 You must verify that all NONMEM installations you intend to
 use can be run directly via the nmfe/NMQual scripts and that they 
 produce complete output files. If you plan to use the parallelization features 
-of NONMEM7.2 you must also verify that you can run NONMEM in parallel directly 
+of NONMEM7.2 or later you must also verify that you can run NONMEM in parallel directly 
 with nmfe72. <p>
 If running NONMEM with nmfe/NMQual does not work then PsN will not work.
 </li>
-<li>Perl5. <br>
-Note 1: For Windows we recommend 
-<a target="_blank" href="http://www.activestate.com/activeperl/downloads/">ActiveState Perl</a>, 
-not Strawberry Perl. Windows users who are already running an older version of PsN do not need to 
-upgrade Perl, but Windows users who are installing PsN for the first time
-need to choose a sub-version of ActiveState Perl
-in which the extra modules needed for PsN are
-available. On Dec 4th 2013 the recommended Perl version was 5.16. For updated information 
-on which version provides which modules see 
+<li>Perl5<p>
+PsN4 requires at least Perl5 version 5.10.1. Please note that users of the free version of ActiveState Perl need an even higher version to be able to download the needed Perl packages (see note below). Users who are already running an older version of PsN should check if they need to upgrade Perl before installing PsN4. The Perl version can be checked with the command <pre>perl --version</pre>
+Note 1: For Windows we recommend
+<a target="_blank" href="http://http://strawberryperl.com/">Strawberry Perl</a> and 
+<a target="_blank" href="http://www.activestate.com/activeperl/downloads/">ActiveState Perl</a>.
+Windows users who are installing PsN for the first time should choose one of Strawberry Perl or ActiveState Perl to install.
+The recommendation is to choose Strawberry Perl.
+If ActiveState is selected it is important to select a version which supports the extra modules needed for PsN.
+On April 3rd 2014 the recommended ActiveState Perl version was 5.16. For updated information 
+on which version provides which modules in ActiveState Perl see 
 package information pages for 
 <a target="_blank" href="http://code.activestate.com/ppm/Math-Random/">Math::Random</a>, 
 <a target="_blank" href="http://code.activestate.com/ppm/File-Copy-Recursive/">File::Copy::Recursive</a>, 
-<a target="_blank" href="http://code.activestate.com/ppm/Archive-Zip/">Archive::Zip</a> 
+<a target="_blank" href="http://code.activestate.com/ppm/Archive-Zip/">Archive::Zip</a>,
+<a target="_blank" href="http://code.activestate.com/ppm/MooseX-Params-Validate">MooseX::Params::Validate</a> 
 and
 <a target="_blank" href="http://code.activestate.com/ppm/Statistics-Distributions/">Statistics::Distributions</a> </br>
 Note 2: Mac users should use the Perl version that is in MacOS by default, not ActiveState.</li>
@@ -37,8 +39,17 @@ but some scm/vpc features and a scm bugfix will not be available.</li>
 </ul>
 </p>
 
-<h3>Installing Math::Random, Statistics::Distributions, Archive::Zip, File::Copy::Recursive</h3>
+<h3>Installing Perl modules</h3>
 You must have internet access for the installation procedure to work.
+<h4>Windows (Strawberry Perl)</h4>
+<ol>
+<li>Start a DOS command window.</li>
+<li>Run the following commands (hit Enter after each command):<br>
+cpan Math::Random<br>
+cpan Statistics::Distributions<br>
+cpan MooseX::Params::Validate<br>
+</li>
+</ol>
 <h4>Windows (ActivePerl)</h4>
 <ol>
 <li>Start a DOS command window.</li>
@@ -47,18 +58,21 @@ ppm install math-random<br>
 ppm install statistics-distributions<br>
 ppm install archive-zip<br>
 ppm install file-copy-recursive<br>
+ppm install moosex-params-validate
 </li>
 </ol>
 <h4>Linux/Unix/MacOS</h4>
-You might need to answer some questions interactively when using the cpan -i command.
+You might need to answer some questions interactively when using the cpan command.
 <ol>
 <li>Start a command window.</li>
 <li>Run the following commands (hit Enter after each command), prepend sudo if necessary:<br>
-cpan -i Math::Random<br>
-cpan -i Statistics::Distributions<br>
-cpan -i Archive::Zip<br>
-cpan -i File::Copy::Recursive<br>
-cpan -i Storable<br>
+cpan Math::Random<br>
+cpan Statistics::Distributions<br>
+cpan Archive::Zip<br>
+cpan File::Copy::Recursive<br>
+cpan Storable<br>
+cpan Moose<br>
+cpan MooseX::Params::Validate<br>
 </li>
 </ol>
 
