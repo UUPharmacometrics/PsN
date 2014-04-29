cedf80cf76abf8a9503068b69318fe2ac1cd65db
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2014-04-29 10:50:03 +0200

    Check if $INPUT contains any lowercase letters in reserved words NONMEM 7.2 and up supports lowercase letters in reserved words in $INPUT. PsN cannot yet support this so it now croaks if this is encountered.

--- lib/model/problem/input.pm
+++ lib/model/problem/input.pm
@@ -2,8 +2,27 @@ package model::problem::input;
 
 use Moose;
 use MooseX::Params::Validate;
+use ext::Carp;
 
 extends 'model::problem::record';
+
+sub BUILD
+{
+	my $self = shift;
+
+	# Check if there are any reserved words containing lower case. Starting with NONMEM 7.2 this is allowed but PsN does not support it
+	foreach my $option (@{$self->options}) {
+		foreach my $string ($option->name, $option->value) {
+			if ($string =~ /ID|L1|L2|DV|MDV|RAW_|MRG_|RPT_|TIME|DATE|DAT1|DAT2|DAT3|DROP|SKIP|EVID|AMT|RATE|SS|II|ADDL|CMT|PCMT|CALL|CONT/i) {
+				if ($string =~ /[a-z]/) {
+					croak("\$INPUT contains a NONMEM reserved word \"$string\" containing lowercase letters. This is not yet supported by PsN." .
+						" Please use all uppercase letters.");
+				}
+			}
+		}
+	}
+}
+
 
 sub get_nonskipped_columns
 {
