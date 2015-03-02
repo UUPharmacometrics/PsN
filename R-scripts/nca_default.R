2c881206308eef22d2e59ed0a0a8f165a6ad71a9
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2015-03-02 14:41:54 +0100

    remove old R script from bin/nca and start new

---
+++ R-scripts/nca_default.R
@@ -0,0 +1,15 @@
+require(xpose4)
+
+if (rplots.level > 0) {
+    pdf(file=pdf.filename, width=10, height=7, title=pdf.title)
+}
+
+
+if (rplots.level > 0) {
+
+}
+
+if (rplots.level > 0){
+    dev.off()
+}
+
