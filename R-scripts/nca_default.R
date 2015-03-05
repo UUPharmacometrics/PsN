60a98e3fa4cc0e41f74a894cf751ed7c717a2f4a
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2015-03-05 08:23:42 +0100

    updated rplots for nca and pvar

--- R-scripts/nca_default.R
+++ R-scripts/nca_default.R
@@ -1,12 +1,18 @@
 library(ggplot2)
+library(ncappc)
+library(gridExtra)
 
 if (rplots.level > 0) {
     pdf(file=pdf.filename, width=10, height=7, title=pdf.title)
 }
 
-
 if (rplots.level > 0) {
+    data = read.table("nca_original.npctab.dta", header=TRUE, skip=1)
 
+    colnames(data)[names(data) == "TIME"] = "Time"
+    colnames(data)[names(data) == "DV"] = "Conc"
+
+    dv.plot(data)
 }
 
 if (rplots.level > 0){
--- R-scripts/pvar_default.R
+++ R-scripts/pvar_default.R
@@ -1,24 +1,24 @@
 if (rplots.level > 0) {
-	pdf(file=pdf.filename, width=10, height=7, title=pdf.title)
-	par(mfrow=c(2,2))
+    pdf(file=pdf.filename, width=10, height=7, title=pdf.title)
+    par(mfrow=c(2,2))
 
-	result = read.csv("result.csv", as.is=TRUE)
+    result = read.csv("result.csv", as.is=TRUE)
 
-	models = result$Model.name
-	models = models[seq(1, length(models), 3)]   # Extract every third
-	models = gsub("\\.(mod|ctl)$", "", models)
+    models = result$Model.name
+    models = models[seq(1, length(models), 3)]   # Extract every third
+    models = gsub("\\.(mod|ctl)$", "", models)
 
 
-	for (i in 4 : ncol(result)) {
-  	values = result[[i]]
-  	values = values[-seq(3, length(values), 3)]  # Remove PV
-  	dim(values) = c(2, length(values) / 2)      # make into 2 x n matrix
-  	colnames(values) = models
-  	rownames(values) = c("EPV", "UPV")
+    for (i in 4 : ncol(result)) {
+        values = result[[i]]
+        values = values[-seq(3, length(values), 3)]  # Remove PV
+        dim(values) = c(2, length(values) / 2)      # make into 2 x n matrix
+        colnames(values) = models
+        rownames(values) = c("EPV", "UPV")
 
-  	barplot(values, xlab="Model", ylab="Variability", legend=TRUE, main=colnames(result)[i])
-  	abline(h=0)
-	}
+        barplot(values, xlab="Model", ylab="Variability", legend=TRUE, main=colnames(result)[i])
+        abline(h=0)
+    }
 
-	dev.off()
+    dev.off()
 }
