01549df843b0e37235a29315c0aa6ea65393a8aa
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2015-04-07 13:45:14 +0200

    bump revision

--- lib/PsN.pm
+++ lib/PsN.pm
@@ -7,7 +7,7 @@ use Config;
 #the version line is extracted in Makefile using regular expression
 # /\$version\s*=\s*.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*.;/
 # so be careful when you edit!!!
-$version = '4.3.20';
+$version = '4.3.21';
 
 # The following line will be changed by make
 $dev = 1;
