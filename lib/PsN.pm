ec8dc8336a4d2f060fb8357f8df8a83021cefe67
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2015-05-07 11:52:25 +0200

    revision 4.4.1

--- lib/PsN.pm
+++ lib/PsN.pm
@@ -7,7 +7,7 @@ use Config;
 #the version line is extracted in Makefile using regular expression
 # /\$version\s*=\s*.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*.;/
 # so be careful when you edit!!!
-$version = '4.4.0';
+$version = '4.4.1';
 
 # The following line will be changed by make
 $dev = 1;
