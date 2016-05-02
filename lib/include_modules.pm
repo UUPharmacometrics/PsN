815189813430d6494203678f188329f37a3a8f1a
Kajsa Harling <kajsa.harling@farmbio.uu.se>
2016-05-02 09:20:21 +0200

    include_modules: new global sub debugmessage to use instead of carp for debuginfo

--- lib/include_modules.pm
+++ lib/include_modules.pm
@@ -2,6 +2,15 @@ package include_modules;
 use ext::Carp;
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(cluck longmess shortmess carp croak);
+our @EXPORT = qw(cluck longmess shortmess carp croak debugmessage);
+
+our $debuglevel=0;
+sub debugmessage{
+	my $messagelevel=shift;
+	my $message = shift;
+	if ($messagelevel <= $debuglevel){
+		carp($message);
+	} 
+}
 
 1;
