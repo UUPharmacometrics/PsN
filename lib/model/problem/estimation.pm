32ea873e08bb2be604e9d26078ee82705862733b
Kajsa Harling <kajsa.harling@farmbio.uu.se>
2016-06-15 12:58:38 +0200

    model-estimation: subroutines for checking if classical method, if option MCETA applicable, what estimation method.

--- lib/model/problem/estimation.pm
+++ lib/model/problem/estimation.pm
@@ -5,6 +5,62 @@ use MooseX::Params::Validate;
 
 extends 'model::problem::record';
 
+sub get_method{
+	my $self = shift;
+
+	my $method='0'; #this is the default, FO
+	my @options = defined($self->options) ? @{$self->options} : ();
+	foreach my $opt (@options){
+		if (defined $opt and ((uc($opt->name) eq 'METHOD') || index('METHOD',uc($opt ->name )) == 0)){
+			$method = uc($opt->value) if (defined $opt->value and length($opt->value)>0);
+		}
+	}
+	return $method;
+}
+
+sub accepts_mceta{
+	my $self = shift;
+
+	my $method=$self->get_method;
+	my $accepts_mceta;
+	if ($method eq '0' or
+		$method =~ /^ZE/ or
+		$method =~ /^FO/ or
+		$method =~ /^CON/ or
+		$method eq '1' or
+		$method =~ /^HYB/ or
+		$method =~ /^ITS/ or
+		$method =~ /^IMP/ or
+		$method =~ /^MAP/ 
+ 		){
+		$accepts_mceta = 1;
+	}else{
+		#BAYES,SAEM,DIRECT
+		$accepts_mceta = 0;
+	}
+	return $accepts_mceta;
+}
+
+sub is_classical{
+	my $self = shift;
+
+	my $method=$self->get_method;
+
+	my $classical;
+	if ($method eq '0' or
+		$method =~ /^ZE/ or
+		$method =~ /^FO/ or
+		$method =~ /^CON/ or
+		$method eq '1' or
+		$method =~ /^HYB/ 
+		){
+		$classical = 1;
+	}else{
+		$classical = 0;
+	}
+	return $classical;
+}
+
 sub renumber_msfo{
 	my $self = shift;
 	my %parm = validated_hash(\@_,
