f78a9e99186c250082e8c831ccc10a2d18c4f5d7
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-08-29 14:48:25 +0200

    proseval: Create results.csv

--- lib/tool/proseval.pm
+++ lib/tool/proseval.pm
@@ -13,6 +13,8 @@ extends 'tool';
 
 has 'model' => ( is => 'rw', isa => 'model' ); 
 has 'evid_column' => ( is => 'rw', isa => 'Int' ); 
+has 'numobs_indiv' => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
+has 'sorted_indiv_numobs' => ( is => 'rw', isa => 'ArrayRef', default => sub { [] } );
 
 sub BUILD
 {
@@ -36,14 +38,14 @@ sub modelfit_setup
     my $ignoresign = defined $self->model->ignoresigns ? $self->model->ignoresigns->[0] : '@';
 
     my $n = 1;
-    my $continue;
+    my $continue = 1;
 
 
     $self->model->set_option(record_name => 'estimation', option_name => 'MAXEVALS', fuzzy_match => 1, option_value => '0');
 	$self->tools([]) unless defined $self->tools;
     my @models_to_run;
 
-    while (1) {
+    while ($continue) {
         my $data = data->new(
             filename => $orig_data_file,
             ignoresign => $ignoresign,
@@ -52,7 +54,6 @@ sub modelfit_setup
         );
 
         $continue = $self->set_evid(dataset => $data, numzeros => $n, evid_column => $self->evid_column);
-        last if not $continue;
 
         my $data_filename = "m1/proseval_$n.dta";
         my $model_filename = $self->directory . "m1/proseval_$n.mod";
@@ -70,6 +71,13 @@ sub modelfit_setup
         $modified_model->_write(filename => $model_filename, relative_data_path => 1);
         push @models_to_run, $modified_model;
         $n++;
+
+        if (not $continue) {
+            # Final iteration. Sort numobs before dataset goes out of scope
+            for my $individual (@{$data->individuals}) {
+                push @{$self->sorted_indiv_numobs}, $self->numobs_indiv->{$individual->idnumber};
+            }
+        }
     }
 
 	my $modelfit = tool::modelfit->new(
@@ -82,6 +90,32 @@ sub modelfit_setup
 	);
 
 	push(@{$self->tools}, $modelfit);
+}
+
+sub modelfit_analyze
+{
+    # Collect all tables into one results table
+
+    my $self = shift;
+
+    open my $fh, '>', "results.csv";
+    print $fh "ID,TIME,EVID,WRES,IWRES,OBS\n";
+
+    for my $ind (0 .. scalar(@{$self->sorted_indiv_numobs}) - 1) {
+        for my $numobs (1 .. $self->sorted_indiv_numobs->[$ind]) {
+            my $data = data->new(
+                filename => "m1/proseval_$numobs.tab",
+                missing_data_token => $self->missing_data_token,
+                idcolumn => $self->model->idcolumn(),
+            );
+            my $individual = $data->individuals->[$ind];
+            for my $line (@{$individual->subject_data}) {
+                print $fh "$line,$numobs\n";
+            }
+        }
+    }
+
+    close $fh;
 }
 
 sub set_evid
@@ -103,6 +137,7 @@ sub set_evid
 
     for my $individual (@{$dataset->individuals}) {
         my $skipped_zeros = 0;
+        my $indiv_set_evid_two = 0;     # Did we set any evid to two for this individual?
         for my $row (@{$individual->subject_data}) {
             my @a = split /,/, $row;
             if ($a[$evid_column] == 0) {
@@ -110,10 +145,14 @@ sub set_evid
                     $skipped_zeros++;
                 } else {
                     $set_evid_two = 1;
+                    $indiv_set_evid_two = 1;
                     $a[$evid_column] = 2;
                     $row = join ',', @a;
                 }
             }
+        }
+        if (not $indiv_set_evid_two and not exists $self->numobs_indiv->{$individual->idnumber}) {
+            $self->numobs_indiv->{$individual->idnumber} = $numzeros;
         }
     }
 
