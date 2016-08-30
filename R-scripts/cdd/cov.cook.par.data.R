54f64de35ef9ff14989836adc1adc09530c415a4
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2016-08-30 17:14:04 +0200

    R-scripts:cdd: Search columns with the specific name

--- R-scripts/cdd/cov.cook.par.data.R
+++ R-scripts/cdd/cov.cook.par.data.R
@@ -1,6 +1,6 @@
 cov.cook.par.data <- function(cdd.data.all) {
    # create data frame of the cook.par data
-   cook.par.data <- cdd.data.all[ , grepl("cook.par.", names(cdd.data.all)) & !grepl(".cook.par.", names(cdd.data.all))]
+   cook.par.data <- cdd.data.all[ , grepl("^cook.par.", names(cdd.data.all))]
    cook.par.data <- subset(cook.par.data[-1,])
    row.names(cook.par.data) <- NULL
    # get names of all parameters
@@ -11,7 +11,7 @@ cov.cook.par.data <- function(cdd.data.all) {
    names.cov.par.data <- c()
    for (i in 1:length(parameters)) {
      # choose needed columns
-     se.par.data.col <- as.vector(cdd.data.all[ , grepl("se.", names(cdd.data.all)) & grepl(parameters[i], names(cdd.data.all))])
+     se.par.data.col <- as.vector(cdd.data.all[ , grep(paste0("^se",paste0(parameters[i],"$")), names(cdd.data.all))])
      # calculate covariances for each parameter
      cov.par.data.col[,i] <- as.vector(se.par.data.col/se.par.data.col[1])
      names.cov.par.data[i] <- paste0("cov.par.",parameters[i])
