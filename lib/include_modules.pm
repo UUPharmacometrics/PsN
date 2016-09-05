18bee5c35bcaf5a103dc7808793dbf42491cb3f1
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-09-05 09:13:41 +0200

    add global function warn_once to output specific warnings
    the warnings are given a name, and will be output only once
    per PsN run.

--- lib/include_modules.pm
+++ lib/include_modules.pm
@@ -3,7 +3,7 @@ use Moose::Util::TypeConstraints;
 use ext::Carp;
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(cluck longmess shortmess carp croak debugmessage);
+our @EXPORT = qw(cluck longmess shortmess carp croak debugmessage warn_once);
 
 our $debuglevel=0;
 sub debugmessage{
@@ -14,9 +14,24 @@ sub debugmessage{
 	} 
 }
 
+my %one_time_warnings;
+
+sub warn_once
+{
+    # Takes two arguments. First the type of warning. Second the warning message
+    my $type = shift;
+    my $message = shift;
+
+    if (not exists $one_time_warnings{$type}) {
+        $one_time_warnings{$type}++;
+        warn($message);
+    }
+}
+
 subtype 'PositiveInt',
 	as 'Int',
 	where { $_ > 0 },
 	message { "This number ($_) is not a positive integer" };
 no Moose::Util::TypeConstraints;
+
 1;
