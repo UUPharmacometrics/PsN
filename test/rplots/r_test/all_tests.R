5f0ffca5bfa45c0a1ec27e856fba8dd7550502c8
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2016-09-07 13:03:12 +0200

    r_test: Add one more test.

--- test/rplots/r_test/all_tests.R
+++ test/rplots/r_test/all_tests.R
@@ -1,6 +1,11 @@
 library(testthat)
+# Used libraries
+library(PerformanceAnalytics)
+require("PEIP")
+# Test files
 test_file("cdd/test_cdd_1.R")
 test_file("cdd/test_cdd_2.R")
 test_file("simeval/test_ebe_npde.R")
 test_file("simeval/test_cwres_iwres.R")
 test_file("simeval/test_ofv.R")
+test_file("simeval/test_outlier_report_table.R")
