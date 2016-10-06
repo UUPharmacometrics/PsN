8265ccf50c6592f8beb6df631285bce0125b1708
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2016-10-06 08:39:52 +0200

    R-scipts and tests:npfit: Add tests, fix functions

--- R-scripts/npfit/data.npfit.R
+++ R-scripts/npfit/data.npfit.R
@@ -1,12 +1,9 @@
 data.npfit <- function(raw.nonparametric.file) {
-  make_pdf <- TRUE
+
   raw_nonparametric_real <- read.csv(raw.nonparametric.file)
   # delete rows with NA values
   raw_nonparametric <- raw_nonparametric_real[complete.cases(raw_nonparametric_real),]
+  rownames(raw_nonparametric)=NULL
   
-  if (nrow(raw_nonparametric) == 0) {
-    print("Don't have npofv values, can't create a plot.")
-    make_pdf <- FALSE
-  }
-  return(list(make_pdf=make_pdf,raw_nonparametric=raw_nonparametric))
+  return(raw_nonparametric)
 }
--- R-scripts/npfit_default.R
+++ R-scripts/npfit_default.R
@@ -4,10 +4,11 @@ library(ggplot2)
 source(paste0(rscripts.directory, "/npfit/data.npfit.R"))
 source(paste0(rscripts.directory, "/npfit/plot.npfit.R"))
 
-list_npfit <- data.npfit(raw.nonparametric.file)
-make_pdf <- list_npfit$make_pdf
-raw_nonparametric <- list_npfit$raw_nonparametric
-if (make_pdf) {
+raw_nonparametric <- data.npfit(raw.nonparametric.file)
+
+if (nrow(raw_nonparametric) == 0) {
+  print("Don't have npofv values, can't create a plot.")
+} else {
   # create pdf file
   pdf(file=pdf.filename,width=11.69, height=8.27)
   
