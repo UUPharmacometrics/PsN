50b2348ff1ff9b682b69403f25029402146f2f2b
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2016-10-21 11:18:35 +0200

    R-plots:simeval: Plots table with two level column names

--- R-scripts/simeval/pdf.all.outlier.report.R
+++ R-scripts/simeval/pdf.all.outlier.report.R
@@ -11,7 +11,21 @@ pdf.all.outlier.report <- function(report.file.name,
                                                 residual.outliers.file)
   
   # draw the table 
-  plot.table(all_outlier_table)
+  if((nrow(all_outlier_table) == 1) && (ncol(all_outlier_table)==1)) {
+    plot.table(all_outlier_table)
+  } else {
+    tab <- tableGrob(all_outlier_table, rows=NULL)
+    header <- tableGrob(all_outlier_table[1, 1:2], rows=NULL, cols=c("Individual level", "Observation level")) 
+    
+    jn <- combine(header[1,], tab, along=2)
+    jn$widths <- rep(max(jn$widths), length(jn$widths)) # make column widths equal
+
+    # change the relevant rows of gtable
+    jn$layout[1:4 , c("l","r")] <- list(c(2,4),c(3,5))
+    
+    # grid.newpage()
+    grid.draw(jn)
+  }
   
   #close pdf file
   dev.off()
