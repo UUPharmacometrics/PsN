535336c95d5b781a2c099c61d2cfe59215a55e9d
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2016-10-25 10:48:16 +0200

    R-scripts:simeval: deal with NA values in the input table

--- R-scripts/simeval/ebe.npde.input.data.R
+++ R-scripts/simeval/ebe.npde.input.data.R
@@ -1,7 +1,8 @@
 input.data <- function(ebe.npde.file,iiv.eta.names) {
-  # read in 
+  # read in data
   ebenpde_tmp <- read.csv(ebe.npde.file) # load csv file
-  n.subjects <- nrow(ebenpde_tmp) # amount of samples
+  n.subjects <- nrow(ebenpde_tmp)
+  
   # rename ETA(n) to ETA.n. because in dataframe names of ETA columns are with dots
   iiv.eta.names <- gsub("\\(",".",iiv.eta.names)
   iiv.eta.names <- gsub("\\)",".",iiv.eta.names)
@@ -9,7 +10,9 @@ input.data <- function(ebe.npde.file,iiv.eta.names) {
   ebenpde_obs <- ebenpde_tmp[,iiv.eta.names]
   if(class(ebenpde_obs) == "numeric") {
     ebenpde_obs <- as.data.frame(ebenpde_obs)
+    colnames(ebenpde_obs) <- iiv.eta.names
   }
+
   #create ebenpde_tmp data frame
   ebenpde_tmp <- ebenpde_tmp[,-grep("ETA.",colnames(ebenpde_tmp))]
   ebenpde_tmp <- cbind(ebenpde_tmp,ebenpde_obs)
@@ -17,6 +20,7 @@ input.data <- function(ebe.npde.file,iiv.eta.names) {
   out <- list(ebenpde_tmp=ebenpde_tmp,
               n.subjects=n.subjects,
               ebenpde_obs=ebenpde_obs,
-              iiv.eta.names=iiv.eta.names)
+              iiv.eta.names=iiv.eta.names,
+              case='')
   return(out)
 }
