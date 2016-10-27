24a190ab69d3e275a06f91c345fdf4a67f2d5a34
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2016-10-27 11:45:14 +0200

    R-scripts:simeval: No NA values will be found

--- R-scripts/simeval/ebe.npde.summary.table.R
+++ R-scripts/simeval/ebe.npde.summary.table.R
@@ -4,11 +4,10 @@ summary.table.ebe.npde <- function(ebenpde_obs,iiv.eta.names) {
   mymean <- c()
   p_mean_not_0 <- c()
   p_var_not_1 <- c()
-  amount_NA_rows <- c()
+  eta_amount <- c()
   for(i in 1:length(iiv.eta.names)){ 
-    # NA_rows <- length()
-    amount_NA_rows[i] <- sum(is.na(ebenpde_obs[,i]))
     vect <- ebenpde_obs[!is.na(ebenpde_obs[,i]),i]
+    eta_amount[i] <- length(vect)
     variance[i] <- var(vect)
     mymean[i] <- mean(vect)
     p_mean_not_0[i] <- wilcox.test(vect)$p.value # $p.value (it means that we look only on p-value from this test)
@@ -17,16 +16,12 @@ summary.table.ebe.npde <- function(ebenpde_obs,iiv.eta.names) {
   
   #ETAs are not sorted if any iov eta, use iiv.eta.names from preamble
   mydataframe <- data.frame('EBE NPDE' = iiv.eta.names,
+                            'Amount of ETAs' = eta_amount,
                             mean = sprintf("%.5f",mymean),
                             'p-value (H_0: mean==0)'=sprintf("%.3f",p_mean_not_0),
                             variance=sprintf("%.5f",variance),
                             'p-value (H_0: var==1)'=sprintf("%.3f",p_var_not_1),
                             check.names=FALSE)
-  if (any(amount_NA_rows > 0)) {
-    mydataframe <- cbind(mydataframe,amount_NA_rows)
-    names(mydataframe) <- c('EBE NPDE','mean','p-value (H_0: mean==0)','variance',
-                            'p-value (H_0: var==1)','Amount of deleted IDs')
-  }
-  
+
   return(mydataframe)
 }
