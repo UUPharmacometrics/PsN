9b642ed3391cd38c318578a2a9d4d065aaa55668
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2016-11-08 11:10:06 +0100

    R-scripts:simeval: Add two headers to all outlier table

--- R-scripts/simeval/pdf.simeval.R
+++ R-scripts/simeval/pdf.simeval.R
@@ -395,21 +395,40 @@ pdf.simeval <- function(ebe.npde.file,iiv.eta.names,iov.eta.names,outlying_crite
   all_outlier_table <- all.outlier.report.table(ofv_outliers,ebe.npde_outliers=ebe.npde_outliers_iiv,cwres.iwres_outliers,ID_deleted)
   
   # draw the table 
-  # if((nrow(all_outlier_table) == 1) && (ncol(all_outlier_table)==1)) {
+  if((nrow(all_outlier_table) == 1) && (ncol(all_outlier_table)==1)) {
     plot.table(all_outlier_table)
-#   } else {
-#     tab <- tableGrob(all_outlier_table, rows=NULL)
-#     header <- tableGrob(all_outlier_table[1, 1:3], rows=NULL, cols=c("","Individual level", "Observation level")) 
-#       
-#     jn <- combine(header[1,], tab, along=2)
-#     # jn$widths <- rep(max(jn$widths), length(jn$widths)) # make column widths equal
-#       
-#     # change the relevant rows of gtable
-#     jn$layout[1:6 , c("l","r")] <- list(c(1,2,4),c(1,3,5))
-#       
-#     grid.newpage()
-#     grid.draw(jn)
-#   }
+  } else {
+    total_rows_per_page <- 20
+    start_row <- 1
+    if (total_rows_per_page > nrow(all_outlier_table)) {
+      end_row <- nrow(all_outlier_table)
+    } else {
+      end_row <- total_rows_per_page
+    }
+    for (i in 1:ceiling(nrow(all_outlier_table)/total_rows_per_page)){
+      all_outlier_table_part <- all_outlier_table[start_row:end_row,]
+      start_row <- end_row + 1
+      if((total_rows_per_page + end_row) < nrow(all_outlier_table)){
+        end_row <- total_rows_per_page + end_row
+      }else {
+        end_row <- nrow(all_outlier_table)
+      }
+      
+      tab <- tableGrob(all_outlier_table_part, rows=NULL)
+      header <- tableGrob(all_outlier_table_part[1, 1:3], rows=NULL, cols=c("","Individual level", "Observation level")) 
+      
+      jn <- combine(header[1,], tab, along=2)
+      # jn$widths <- rep(max(jn$widths), length(jn$widths)) # make column widths equal
+      
+      # change the relevant rows of gtable
+      jn$layout[1:6 , c("l","r")] <- list(c(1,2,4),c(1,3,5))
+      
+      grid.newpage()
+      grid.draw(jn) 
+      
+    }
+    
+  }
   
   
 #   all_outlier_table <- all.outlier.report.table(ofv_outliers,cwres.iwres_outliers,list_ebe_outlier_table,cases,deleted_ID)
