29b96678368779eaee6a9d4b783f52c09dfb3d54
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2016-11-22 10:31:23 +0100

    frem: revert plot code

--- R-scripts/frem/parameter_ratio_function.R
+++ R-scripts/frem/parameter_ratio_function.R
@@ -3,116 +3,113 @@ parameter_ratio <- function(inTable_frem,covdata,pardata,file_format) {
   if (missing(file_format)) {
     file_format <- c("png")
   }
-#   # check if there are all 3 input data files
-#   if (exists("inTable_frem") & exists("covdata") & exists("pardata")) {
+  # check if there are all 3 input data files
+  if (exists("inTable_frem") & exists("covdata") & exists("pardata")) {
     library(grid)
     library(gridExtra)
     library(reshape2)
     library(dplyr)
     library(ggplot2)
 
-###############################   delete_prefix.R   ###########################################    
-#     # names of covariate (names of first column in covdata input table, header = FALSE)
-#     covariate <- as.character(covdata[[1]])
-#     
-#     # delete "LN" prefixes (if they exist) from any of covariates
-#     for (i in 1:length(covariate)) {
-#       if (grepl("^LN", covariate[i])) {
-#         covariate[i] <- gsub("\\LN","",covariate[i])
-#       }
-#     }
-#    
+    # colnames of frem data
+    colnames_frem <- colnames(inTable_frem)
+    # names of covariate (names of first column in covdata input table, header = FALSE)
+    covariate <- as.character(covdata[[1]])
+    
+    # delete "LN" prefixes (if they exist) from any of covariates
+    for (i in 1:length(covariate)) {
+      if (grepl("^LN", covariate[i])) {
+        covariate[i] <- gsub("\\LN","",covariate[i])
+      }
+    }
+   
+    # number of covariates
+    iNumCovs <- length(covariate)
 
-################################    param_round_values.R   ########################################
-#     # round values to reasonable amount of significant figures (4 as maximum)
-#     for (i in 1:nrow(covdata)) {
-#       if (covdata$is.categorical[i] == "0") {
-#         # covdata contains percentiles and means
-#         covdata[i, 2:5] <- signif(covdata[i, 2:5], digits=4)
-#       }
-#     }
+    # round values to reasonable amount of significant figures (4 as maximum)
+    for (i in 1:nrow(covdata)) {
+      if (covdata$is.categorical[i] == "0") {
+        # covdata contains percentiles and means
+        covdata[i, 2:5] <- signif(covdata[i, 2:5], digits=4)
+      }
+    }
 
-################################   param_values_for_plots.R   ################################
-#     # CREATE VECTOR OF POINT (MEAN) NAMES IN PLOT -----------------------------
-#     list_v <- list() # for point names in plot
-#     list_color <- list() # for point colors in plot
-#     list_mean <- list() # for text in left side of plot
-#     list_covariate <- list()
-# 
-#     for (v in 1:nrow(covdata)) {
-#       if (covdata$is.categorical[v] != "1") {
-#         r <- rbind(covdata$perc5th[v],covdata$perc95th[v])
-#         p_color <- rbind("cov5th", "cov95th")
-#         m <- rbind(paste0(format(signif(covdata$mean[v], digits=2))," ",covdata$unit[v]),"")
-#         c <- rbind(covariate[v],"")
-#       } else {
-#         r <- covdata$category.other[v]
-#         p_color <- "other"
-#         m <- covdata$category.reference[v]
-#         c <- covariate[v]
-#       }
-#       list_v[v] <- list(r)
-#       list_color[v] <- list(p_color)
-#       list_mean[v] <- list(m)
-#       list_covariate[v] <- list(c)
-#     }
-#     point_names <- as.vector(do.call(rbind,list_v))
-#     point_color <- as.vector(do.call(rbind,list_color))
-#     MEAN <- as.vector(do.call(rbind,list_mean))
-#     COVARIATE <- as.vector(do.call(rbind,list_covariate))
+    # CREATE VECTOR OF POINT (MEAN) NAMES IN PLOT -----------------------------
+    list_v <- list() # for pont names in plot
+    list_color <- list() # for point colors in plot
+    list_mean <- list() # for text in left side of plot
+    list_covariate <- list()
 
-##############################    parameter_datas.R    #########################################
+    for (v in 1:nrow(covdata)) {
+      if (covdata$is.categorical[v] != "1") {
+        r <- rbind(covdata$perc5th[v],covdata$perc95th[v])
+        p_color <- rbind("cov5th", "cov95th")
+        m <- rbind(paste0(format(signif(covdata$mean[v], digits=2))," ",covdata$unit[v]),"")
+        c <- rbind(covariate[v],"")
+      } else {
+        r <- covdata$category.other[v]
+        p_color <- "other"
+        m <- covdata$category.reference[v]
+        c <- covariate[v]
+      }
+      list_v[v] <- list(r)
+      list_color[v] <- list(p_color)
+      list_mean[v] <- list(m)
+      list_covariate[v] <- list(c)
+    }
+    point_names <- as.vector(do.call(rbind,list_v))
+    point_color <- as.vector(do.call(rbind,list_color))
+    MEAN <- as.vector(do.call(rbind,list_mean))
+    COVARIATE <- as.vector(do.call(rbind,list_covariate))
+
     # SORT NEEDED DATA FOR EACH PARAMETER -------------------------------------
-#     list_part <- list()
-#     list_colnames <- list()
-#     for (j in 1:nrow(pardata)) {
-#       for (i in 1:length(covariate)) {
-#         if (covdata$is.categorical[i] != "1") {
-#           part_5th <- inTable_frem[ , grepl("RATIO.par.", names(inTable_frem)) & grepl(pardata$parname[j], names(inTable_frem)) & grepl("given.cov5th.", names(inTable_frem))  & grepl(covariate[i], names(inTable_frem))]
-#           name_5th <- paste0(covariate[i],".cov5th")
-#           part_95th <- inTable_frem[ , grepl("RATIO.par.", names(inTable_frem)) & grepl(pardata$parname[j], names(inTable_frem)) & grepl("given.cov95th.", names(inTable_frem))  & grepl(covariate[i], names(inTable_frem))]
-#           name_95th <- paste0(covariate[i],".cov95th")
-#           part <- cbind(part_5th,part_95th)
-#           name <- cbind(name_5th,name_95th)
-#           list_part[i] <- list(part)
-#           list_colnames[i] <- list(name)
-#         } else {
-#           part <- inTable_frem[ , grepl("RATIO.par.", names(inTable_frem)) & grepl(pardata$parname[j], names(inTable_frem)) & grepl("given.other.", names(inTable_frem))  & grepl(covariate[i], names(inTable_frem))]
-#           name <- paste0(covariate[i],".other")
-#           list_part[i] <- list(part)
-#           list_colnames[i] <- list(name)
-#         }
-#       }
-#       ColNames <- unlist(list_colnames)
-#       DF <- do.call(cbind,list_part) # matrix for each parameter
-#       colnames(DF) <- ColNames
-#       DF <- as.data.frame(DF) # data frame for each parameter with column names of covariate
+    list_part <- list()
+    list_colnames <- list()
+    for (j in 1:nrow(pardata)) {
+      for (i in 1:length(covariate)) {
+        if (covdata$is.categorical[i] != "1") {
+          part_5th <- inTable_frem[ , grepl("RATIO.par.", names(inTable_frem)) & grepl(pardata$parname[j], names(inTable_frem)) & grepl("given.cov5th.", names(inTable_frem))  & grepl(covariate[i], names(inTable_frem))]
+          name_5th <- paste0(covariate[i],".cov5th")
+          part_95th <- inTable_frem[ , grepl("RATIO.par.", names(inTable_frem)) & grepl(pardata$parname[j], names(inTable_frem)) & grepl("given.cov95th.", names(inTable_frem))  & grepl(covariate[i], names(inTable_frem))]
+          name_95th <- paste0(covariate[i],".cov95th")
+          part <- cbind(part_5th,part_95th)
+          name <- cbind(name_5th,name_95th)
+          list_part[i] <- list(part)
+          list_colnames[i] <- list(name)
+        } else {
+          part <- inTable_frem[ , grepl("RATIO.par.", names(inTable_frem)) & grepl(pardata$parname[j], names(inTable_frem)) & grepl("given.other.", names(inTable_frem))  & grepl(covariate[i], names(inTable_frem))]
+          name <- paste0(covariate[i],".other")
+          list_part[i] <- list(part)
+          list_colnames[i] <- list(name)
+        }
+      }
+      ColNames <- unlist(list_colnames)
+      DF <- do.call(cbind,list_part) # matrix for each parameter
+      colnames(DF) <- ColNames
+      DF <- as.data.frame(DF) # data frame for each parameter with column names of covariate
 
-  
-#################################   param_plot_data.R   ############################################
-#       # PREPARE DATA FRAME FOR PLOTTING ----------------------------------------
-#       # Create long data set
-#       DF_melt <- melt(DF)
-# 
-#       # summaryze dataframe and calculate mean, quantile for each group (for each column in DF)
-#       outTable <- DF_melt %>% group_by(variable) %>%
-#       summarise(mean = value[1],
-#                 ci_low = quantile(value[-1], probs=c(0.05),type=2),
-#                 ci_high = quantile(value[-1], probs=c(0.95),type=2))
-#       # calculating procentage of outTable
-#       outTablet_proc <- t(round(t((outTable[, 2:ncol(outTable)])-1) * 100, 2))
-#       outTable <- cbind(outTable[1],outTablet_proc)
-#       # add some needed columns for plotting
-#       outTable$EXPECTED<-sprintf("%+.3G %%  [%+.3G, %+.3G]",outTable$mean,outTable$ci_low,outTable$ci_high)
-#       outTable$points <- point_names
-#       outTable$group <- point_color
-#       outTable$MEAN <- MEAN
-#       outTable$COVARIATE <- COVARIATE
-# 
-#       # add an empty row to outTable
-#       empty_row <- c(rep(NA,ncol(outTable)))
-#       outTable <-rbind(empty_row,outTable)
+      # PREPARE DATA FRAME FOR PLOTTING ----------------------------------------
+      # Create long data set
+      DF_melt <- melt(DF)
 
+      # summaryze dataframe and calculate mean, quantile for each group (for each column in DF)
+      outTable <- DF_melt %>% group_by(variable) %>%
+      summarise(mean = value[1],
+                ci_low = quantile(value[-1], probs=c(0.05),type=2),
+                ci_high = quantile(value[-1], probs=c(0.95),type=2))
+      # calculating procentage of outTable
+      outTablet_proc <- t(round(t((outTable[, 2:ncol(outTable)])-1) * 100, 2))
+      outTable <- cbind(outTable[1],outTablet_proc)
+      # add some needed columns for plotting
+      outTable$EXPECTED<-sprintf("%+.3G %%  [%+.3G, %+.3G]",outTable$mean,outTable$ci_low,outTable$ci_high)
+      outTable$points <- point_names
+      outTable$group <- point_color
+      outTable$MEAN <- MEAN
+      outTable$COVARIATE <- COVARIATE
+
+      # add an empty row to outTable
+      empty_row <- c(rep(NA,ncol(outTable)))
+      outTable <-rbind(empty_row,outTable)
 
       outTable$y <- factor(c(1:nrow(outTable)), levels=c(nrow(outTable):1))
       # MAKE FOREST PLOT --------------------------------------------------------
@@ -137,13 +134,12 @@ parameter_ratio <- function(inTable_frem,covdata,pardata,file_format) {
         }
       }
 
-###################################    param_plots_text.R   ###################################      
-#       # create table with all needed information
-#       outTable <- outTable[-1,]
-#       outTable_text <- data.frame()
-#       V1 <- c(colnames(outTable)[9], outTable$COVARIATE, colnames(outTable)[8],outTable$MEAN, colnames(outTable)[5], outTable$EXPECTED)
-#       V05 <- rep(c(1:3),each = (nrow(outTable) +1) )
-#       outTable_text <- data.frame(V1,V05,V0 = factor(rep(c(1:(nrow(outTable) +1)),3),levels = c((nrow(outTable) +1):1)))
+      # create table with all needed information
+      outTable <- outTable[-1,]
+      outTable_text <- data.frame()
+      V1 <- c(colnames(outTable)[9], outTable$COVARIATE, colnames(outTable)[8],outTable$MEAN, colnames(outTable)[5], outTable$EXPECTED)
+      V05 <- rep(c(1:3),each = (nrow(outTable) +1) )
+      outTable_text <- data.frame(V1,V05,V0 = factor(rep(c(1:(nrow(outTable) +1)),3),levels = c((nrow(outTable) +1):1)))
 
       # create plot of text table
       data_table <- ggplot(outTable_text,aes(x = V05, y = V0, label = format(V1, nsmall = 1))) +
@@ -178,8 +174,8 @@ parameter_ratio <- function(inTable_frem,covdata,pardata,file_format) {
       ggsave(filename = name, plot = gp, width=11.69, height=8.27)
       dev.off()
     }
-#   } else {
-#     cat("Input data files are not found! Make sore that input data files are in your working directory!")
-#   }
+  } else {
+    cat("Input data files are not found! Make sore that input data files are in your working directory!")
+  }
 }
 
