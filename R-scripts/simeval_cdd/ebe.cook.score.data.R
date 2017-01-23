65b9a3880f0433298bb2d59bbe050964313e28aa
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2017-01-23 11:46:47 +0100

    R-script:cdd.simeval: Add flag if no cook scores or cov ratios

--- R-scripts/simeval_cdd/ebe.cook.score.data.R
+++ R-scripts/simeval_cdd/ebe.cook.score.data.R
@@ -42,124 +42,132 @@ ebe_cook.score_data <- function(ebe.npde.file,eta.names,outlying_criteria,
   out_cdd.data.all <- create.data.full(raw.results.file,skipped.id.file) # use function
   cdd.data.all <- out_cdd.data.all$cdd.data.all
   cdd.data.all <- cdd.data.all[-1,]
+  no.cook.cov <- out_cdd.data.all$no.cook.cov
   
-  # save needed columns in one data frame
-  infl_data <- as.data.frame(cbind(cdd.data.all$ID,cdd.data.all$cook.scores))
-  colnames(infl_data) <- c("ID_cdd","cook_scores")
-  
-  # find influential
-  infl_row <- c()
-  for (i in 1:nrow(infl_data)) {
-    if(infl_data$cook_scores[i] > cutoff_cook) {
-      infl_row <- c(infl_row,i)
-    }
-  }
-  if(length(infl_row)>0) {
-    infl_ID <- infl_data$ID[infl_row]
-  } else {
-    infl_ID <- c()
-  }
-  
-  # cdd and simeval ------------------------------------------------------------------------
-  #delete ID numbers (deleted ID numbers from EBE) from CDD and compute a warning message
-  if(length(ID_deleted_ebe)>0) {
-
-    # delete individuals from cdd which have been deleted from simeval EBE
-    if(any(infl_data$ID %in% ID_deleted_ebe)) {
-      deleted_cdd_ID.ebe <- intersect(ID_deleted_ebe,infl_data$ID)
-      # if there are deleted influentials
-      if(length(infl_ID)>0) {
-        deleted_infl_ID <- intersect(ID_deleted_ebe,infl_ID)
+  if(!no.cook.cov) {
+    # save needed columns in one data frame
+    infl_data <- as.data.frame(cbind(cdd.data.all$ID,cdd.data.all$cook.scores))
+    colnames(infl_data) <- c("ID_cdd","cook_scores")
+    
+    # find influential
+    infl_row <- c()
+    for (i in 1:nrow(infl_data)) {
+      if(infl_data$cook_scores[i] > cutoff_cook) {
+        infl_row <- c(infl_row,i)
       }
-      # message
-      if(length(deleted_cdd_ID.ebe)>0) {
-        deleted_cdd_ID.ebe_text <- c()
-        for (i in 1:length(deleted_cdd_ID.ebe)) {
-          if(i == 1) {
-            deleted_cdd_ID.ebe_text <- deleted_cdd_ID.ebe[i]
-          } else {
-            deleted_cdd_ID.ebe_text <- paste0(deleted_cdd_ID.ebe_text,", ",deleted_cdd_ID.ebe[i])
-          }
+    }
+    if(length(infl_row)>0) {
+      infl_ID <- infl_data$ID[infl_row]
+    } else {
+      infl_ID <- c()
+    }
+    
+    # cdd and simeval ------------------------------------------------------------------------
+    #delete ID numbers (deleted ID numbers from EBE) from CDD and compute a warning message
+    if(length(ID_deleted_ebe)>0) {
+      
+      # delete individuals from cdd which have been deleted from simeval EBE
+      if(any(infl_data$ID %in% ID_deleted_ebe)) {
+        deleted_cdd_ID.ebe <- intersect(ID_deleted_ebe,infl_data$ID)
+        # if there are deleted influentials
+        if(length(infl_ID)>0) {
+          deleted_infl_ID <- intersect(ID_deleted_ebe,infl_ID)
         }
-        if(exists("deleted_infl_ID")) {
-          deleted_infl_ID_text <- c()
-          for (i in 1:length(deleted_infl_ID)) {
+        # message
+        if(length(deleted_cdd_ID.ebe)>0) {
+          deleted_cdd_ID.ebe_text <- c()
+          for (i in 1:length(deleted_cdd_ID.ebe)) {
             if(i == 1) {
-              deleted_infl_ID_text <- deleted_infl_ID[i]
+              deleted_cdd_ID.ebe_text <- deleted_cdd_ID.ebe[i]
             } else {
-              deleted_infl_ID_text <- paste0(deleted_infl_ID_text,", ",deleted_infl_ID[i])
+              deleted_cdd_ID.ebe_text <- paste0(deleted_cdd_ID.ebe_text,", ",deleted_cdd_ID.ebe[i])
             }
           }
-          message(paste0("WARNING! Deleted individuals ",deleted_cdd_ID.ebe_text," from which individuals ",deleted_infl_ID_text," are influential!"))
-        } else {
-          message(paste0("WARNING! Deleted individuals ",deleted_cdd_ID.ebe_text,"!"))
+          if(exists("deleted_infl_ID")) {
+            deleted_infl_ID_text <- c()
+            for (i in 1:length(deleted_infl_ID)) {
+              if(i == 1) {
+                deleted_infl_ID_text <- deleted_infl_ID[i]
+              } else {
+                deleted_infl_ID_text <- paste0(deleted_infl_ID_text,", ",deleted_infl_ID[i])
+              }
+            }
+            message(paste0("WARNING! Deleted individuals ",deleted_cdd_ID.ebe_text," from which individuals ",deleted_infl_ID_text," are influential!"))
+          } else {
+            message(paste0("WARNING! Deleted individuals ",deleted_cdd_ID.ebe_text,"!"))
+          }
+          
         }
-        
+        #delete rows
+        row_deleted_infl <- c()
+        row_delete_ebe <- c()
+        for(i in 1:length(ID_deleted_ebe)) {
+          row_delete_ebe_i <- which(infl_data$ID %in% ID_deleted_ebe[i])
+          row_delete_ebe <- c( row_delete_ebe,row_delete_ebe_i)
+        }
+        infl_data <- infl_data[-row_delete_ebe,]
+        rownames(infl_data) <- NULL
+        infl_ID <- intersect(infl_ID,infl_data$ID)
       }
-      #delete rows
-      row_deleted_infl <- c()
-      row_delete_ebe <- c()
-      for(i in 1:length(ID_deleted_ebe)) {
-        row_delete_ebe_i <- which(infl_data$ID %in% ID_deleted_ebe[i])
-        row_delete_ebe <- c( row_delete_ebe,row_delete_ebe_i)
+    }
+    
+    # find which individuals are infuencial and outliers
+    ID <- intersect(outlier_ID,infl_ID)
+    
+    #order influential individual table and outlier table by iD numbers
+    ID_ordered_influential_table <- infl_data[order(infl_data$ID_cdd),]
+    rownames(ID_ordered_influential_table) <- NULL
+    ID_ordered_outlier_table <- outl_data[order(outl_data$ID_simeval),]
+    rownames(ID_ordered_outlier_table) <- NULL
+    
+    # create a table for plotting
+    table_for_plot <- as.data.frame(cbind(ID_ordered_influential_table$ID_cdd,ID_ordered_influential_table$cook_scores,ID_ordered_outlier_table$EBE_NPDE))
+    colnames(table_for_plot) <- c("ID","cdd_cook_scores","simeval_EBE_NPDE")
+    
+    #rows of infl and outl individuals
+    row <- c()
+    for (i in 1:length(ID)) {
+      row[i] <- which(table_for_plot$ID %in% ID[i])
+    }
+    
+    # count how many are influential and outliers, influential but not outliers, --------------
+    # outliers but not influential, not outliers and not influential
+    infl_outl <- ID
+    infl_not_outl <- setdiff(infl_ID,ID)
+    outl_not_infl <- setdiff(outlier_ID,ID)
+    not_outl_not_infl <- setdiff(table_for_plot$ID,unique(c(outlier_ID,infl_ID)))
+    
+    #rows of influential indiv but not outliers
+    row_infl_not_outl <- c()
+    if(length(infl_not_outl) >0) {
+      for (i in 1:length(infl_not_outl)) {
+        row_infl_not_outl[i] <- which(table_for_plot$ID %in% infl_not_outl[i])
       }
-      infl_data <- infl_data[-row_delete_ebe,]
-      rownames(infl_data) <- NULL
-      infl_ID <- intersect(infl_ID,infl_data$ID)
     }
-  }
-  
-  # find which individuals are infuencial and outliers
-  ID <- intersect(outlier_ID,infl_ID)
-  
-  #order influential individual table and outlier table by iD numbers
-  ID_ordered_influential_table <- infl_data[order(infl_data$ID_cdd),]
-  rownames(ID_ordered_influential_table) <- NULL
-  ID_ordered_outlier_table <- outl_data[order(outl_data$ID_simeval),]
-  rownames(ID_ordered_outlier_table) <- NULL
-  
-  # create a table for plotting
-  table_for_plot <- as.data.frame(cbind(ID_ordered_influential_table$ID_cdd,ID_ordered_influential_table$cook_scores,ID_ordered_outlier_table$EBE_NPDE))
-  colnames(table_for_plot) <- c("ID","cdd_cook_scores","simeval_EBE_NPDE")
-  
-  #rows of infl and outl individuals
-  row <- c()
-  for (i in 1:length(ID)) {
-    row[i] <- which(table_for_plot$ID %in% ID[i])
-  }
-  
-  # count how many are influential and outliers, influential but not outliers, --------------
-  # outliers but not influential, not outliers and not influential
-  infl_outl <- ID
-  infl_not_outl <- setdiff(infl_ID,ID)
-  outl_not_infl <- setdiff(outlier_ID,ID)
-  not_outl_not_infl <- setdiff(table_for_plot$ID,unique(c(outlier_ID,infl_ID)))
-  
-  #rows of influential indiv but not outliers
-  row_infl_not_outl <- c()
-  if(length(infl_not_outl) >0) {
-    for (i in 1:length(infl_not_outl)) {
-      row_infl_not_outl[i] <- which(table_for_plot$ID %in% infl_not_outl[i])
+    row_outl_not_infl <- c()
+    if(length(outl_not_infl) > 0) {
+      for (i in 1:length(outl_not_infl)) {
+        row_outl_not_infl[i] <- which(table_for_plot$ID %in% outl_not_infl[i])
+      }
     }
-  }
-  row_outl_not_infl <- c()
-  if(length(outl_not_infl) > 0) {
-    for (i in 1:length(outl_not_infl)) {
-      row_outl_not_infl[i] <- which(table_for_plot$ID %in% outl_not_infl[i])
-    }
+    
+    # return
+    out <- list(infl_data=infl_data,
+                outl_data=outl_data,
+                table_for_plot= table_for_plot,
+                ID=ID,
+                row=row,
+                infl_outl=infl_outl,
+                infl_not_outl=infl_not_outl,
+                outl_not_infl=outl_not_infl,
+                not_outl_not_infl=not_outl_not_infl,
+                row_infl_not_outl=row_infl_not_outl,
+                row_outl_not_infl=row_outl_not_infl,
+                no.cook.cov=no.cook.cov)
+    return(out)
+  } else {
+    return(no.cook.cov)
+    message("No cook score or cov ratio values are found in the raw results csv file!")
   }
   
-  # return
-  out <- list(infl_data=infl_data,
-              outl_data=outl_data,
-              table_for_plot= table_for_plot,
-              ID=ID,
-              row=row,
-              infl_outl=infl_outl,
-              infl_not_outl=infl_not_outl,
-              outl_not_infl=outl_not_infl,
-              not_outl_not_infl=not_outl_not_infl,
-              row_infl_not_outl=row_infl_not_outl,
-              row_outl_not_infl=row_outl_not_infl)
-  return(out)
 }
--- R-scripts/simeval_cdd/pdf_outl.infl.indiv.R
+++ R-scripts/simeval_cdd/pdf_outl.infl.indiv.R
@@ -1,7 +1,7 @@
 pdf_outl.infl.indiv <- function(all.iofv.file,n.subjects,samples,raw.results.file,skipped.id.file,
                                 residual.outliers.file,ebe.npde.file,eta.names,
                                 pdf.filename,cutoff_cook,cutoff_delta.ofv) {
-  outlying_criteria <- 3 # no -3, because we we compare max(abs(EBE_NPDE values)) always positive
+  outlying_criteria <- 3 # no -3, because we compare max(abs(EBE_NPDE values)) always positive
   ##################################################    delta odv and simeval OFV  ##########################################################
   # Compare outliers and influential individuals
   outdata <- influential_outliers_data(all.iofv.file,n.subjects,samples,
@@ -32,75 +32,82 @@ pdf_outl.infl.indiv <- function(all.iofv.file,n.subjects,samples,raw.results.fil
   
   list_data <- ebe_cook.score_data(ebe.npde.file,eta.names,outlying_criteria,
                                    raw.results.file,skipped.id.file,cutoff_cook)
-  #unlist
-  par_table_for_plot <- list_data$table_for_plot
-  par_ID <- list_data$ID
-  par_row <- list_data$row
-  par_infl_outl <- list_data$infl_outl
-  par_infl_not_outl <- list_data$infl_not_outl
-  par_outl_not_infl <- list_data$outl_not_infl
-  par_not_outl_not_infl <- list_data$not_outl_not_infl
-  par_row_infl_not_outl <- list_data$row_infl_not_outl
-  par_row_outl_not_infl <- list_data$row_outl_not_infl
-  # plot points, mark outliers and influential individuals ir red
-  plot_param_infl_outl_data(par_table_for_plot,par_ID,par_row,par_row_outl_not_infl,par_row_infl_not_outl,cutoff_cook,outlying_criteria)
   
-  # plot summary about how many are only influential, only outliers, both and none.
-  plot_summary(par_infl_outl,par_infl_not_outl,par_outl_not_infl,par_not_outl_not_infl)
+  if(class("list_data") == "list") {
+    #unlist
+    par_table_for_plot <- list_data$table_for_plot
+    par_ID <- list_data$ID
+    par_row <- list_data$row
+    par_infl_outl <- list_data$infl_outl
+    par_infl_not_outl <- list_data$infl_not_outl
+    par_outl_not_infl <- list_data$outl_not_infl
+    par_not_outl_not_infl <- list_data$not_outl_not_infl
+    par_row_infl_not_outl <- list_data$row_infl_not_outl
+    par_row_outl_not_infl <- list_data$row_outl_not_infl
+    # plot points, mark outliers and influential individuals ir red
+    plot_param_infl_outl_data(par_table_for_plot,par_ID,par_row,par_row_outl_not_infl,par_row_infl_not_outl,cutoff_cook,outlying_criteria)
+    
+    # plot summary about how many are only influential, only outliers, both and none.
+    plot_summary(par_infl_outl,par_infl_not_outl,par_outl_not_infl,par_not_outl_not_infl)
+  } else {
+    no.cook.cov <- list_data
+  }
   
   ###################################################   summary ourlier influential indiv table  ###########################################################
-  table_list <- outlier_infl_table(all.iofv.file,n.subjects,samples,ebe.npde.file,eta.names,outlying_criteria,
-                     residual.outliers.file,raw.results.file,skipped.id.file,cutoff_delta.ofv)
-  all_infl_indiv_table <- table_list$all_infl_indiv_table
-  all_outlier_table <- table_list$all_outlier_table
-  outl_infl_table <- table_list$outl_infl_table
-  col_amount <- table_list$col_amount
-
-  # draw the table 
-  if((nrow(outl_infl_table) == 1) && (ncol(outl_infl_table)==1)) {
-    plot.table(outl_infl_table)
-  } else {
-    total_rows_per_page <- 18
-    start_row <- 1
-    if (total_rows_per_page > nrow(outl_infl_table)) {
-      end_row <- nrow(outl_infl_table)
-    } else {
-      end_row <- total_rows_per_page
-    }
+  if(!no.cook.cov) {
+    table_list <- outlier_infl_table(all.iofv.file,n.subjects,samples,ebe.npde.file,eta.names,outlying_criteria,
+                                     residual.outliers.file,raw.results.file,skipped.id.file,cutoff_delta.ofv)
+    all_infl_indiv_table <- table_list$all_infl_indiv_table
+    all_outlier_table <- table_list$all_outlier_table
+    outl_infl_table <- table_list$outl_infl_table
+    col_amount <- table_list$col_amount
     
-    for (i in 1:ceiling(nrow(outl_infl_table)/total_rows_per_page)){
-      outl_infl_table_part <- outl_infl_table[start_row:end_row,]
-      
-      # add text "continues", if table continues in other pages
-      nr_col <- ncol(outl_infl_table_part)
-      add_vect <- c(rep("",nr_col-1),"continues...")
-      if(i != ceiling(nrow(outl_infl_table)/total_rows_per_page)) {
-        outl_infl_table_part <- rbind(outl_infl_table_part,add_vect)
-      }
-      
-      start_row <- end_row + 1
-      if((total_rows_per_page + end_row) < nrow(outl_infl_table)){
-        end_row <- total_rows_per_page + end_row
-      }else {
+    # draw the table 
+    if((nrow(outl_infl_table) == 1) && (ncol(outl_infl_table)==1)) {
+      plot.table(outl_infl_table)
+    } else {
+      total_rows_per_page <- 18
+      start_row <- 1
+      if (total_rows_per_page > nrow(outl_infl_table)) {
         end_row <- nrow(outl_infl_table)
-      }
-      tab <- tableGrob(outl_infl_table_part, rows=NULL)
-      header <- tableGrob(outl_infl_table_part[1, 1:3], rows=NULL, cols=c("","SIMEVAL outliers", "CDD influentials"))  # extra header
-      
-      jn <- combine(header[1,], tab, along=2)
-      # jn$widths <- rep(max(jn$widths), length(jn$widths)) # make column widths equal
-   
-      # change the relevant rows of gtable
-      if(col_amount==8) {
-        jn$layout[1:6 , c("l","r")] <- list(c(1,2,6),c(1,5,8))
       } else {
-        jn$layout[1:6 , c("l","r")] <- list(c(1,2,4),c(1,3,6))
+        end_row <- total_rows_per_page
       }
       
-      grid.newpage()
-      grid.draw(jn)
+      for (i in 1:ceiling(nrow(outl_infl_table)/total_rows_per_page)){
+        outl_infl_table_part <- outl_infl_table[start_row:end_row,]
+        
+        # add text "continues", if table continues in other pages
+        nr_col <- ncol(outl_infl_table_part)
+        add_vect <- c(rep("",nr_col-1),"continues...")
+        if(i != ceiling(nrow(outl_infl_table)/total_rows_per_page)) {
+          outl_infl_table_part <- rbind(outl_infl_table_part,add_vect)
+        }
+        
+        start_row <- end_row + 1
+        if((total_rows_per_page + end_row) < nrow(outl_infl_table)){
+          end_row <- total_rows_per_page + end_row
+        }else {
+          end_row <- nrow(outl_infl_table)
+        }
+        tab <- tableGrob(outl_infl_table_part, rows=NULL)
+        header <- tableGrob(outl_infl_table_part[1, 1:3], rows=NULL, cols=c("","SIMEVAL outliers", "CDD influentials"))  # extra header
+        
+        jn <- combine(header[1,], tab, along=2)
+        # jn$widths <- rep(max(jn$widths), length(jn$widths)) # make column widths equal
+        
+        # change the relevant rows of gtable
+        if(col_amount==8) {
+          jn$layout[1:6 , c("l","r")] <- list(c(1,2,6),c(1,5,8))
+        } else {
+          jn$layout[1:6 , c("l","r")] <- list(c(1,2,4),c(1,3,6))
+        }
+        
+        grid.newpage()
+        grid.draw(jn)
+      }
     }
   }
-
+  
   dev.off()
 }
