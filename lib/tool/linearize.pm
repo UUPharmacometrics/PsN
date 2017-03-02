be1e3cc8389f908e533575352907069634914ebe
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2017-03-02 15:55:02 +0100

    linearize: add -full_block option

--- lib/tool/linearize.pm
+++ lib/tool/linearize.pm
@@ -5,11 +5,13 @@ use MooseX::Params::Validate;
 use File::Path;
 use File::Copy 'cp';
 use tool;
+use tool::modelfit;
 use tool::scm;
 
 has 'epsilon' => ( is => 'rw', isa => 'Bool', default => 1 );
 has 'foce' => ( is => 'rw', isa => 'Bool', default => 1 );
 has 'error' => ( is => 'rw', isa => 'Maybe[Str]' );
+has 'full_block' => ( is => 'rw', isa => 'Bool' );      # Set to also run with full block OMEGA
 
 extends 'tool';
 
@@ -80,6 +82,59 @@ sub modelfit_setup
 
     cp($scm->basename . '.dta', '../' . $scm->basename . '.dta');
     cp($scm->basename . '.mod', '../' . $scm->basename . '.mod');
+
+    if ($self->full_block) {
+        my $name = $scm->basename;
+        $name =~ s/linbase/full_block.mod/;
+        my $full_block_model = $self->_create_full_block(input_model_name => $scm->basename . '.mod', output_model_name => "$name");
+
+        my $modelfit = tool::modelfit->new(
+            %{common_options::restore_options(@common_options::tool_options)},
+		    models => [ $full_block_model ], 
+            base_dir => $self->directory . 'm1/',
+            directory => undef,
+            top_tool => 0,
+            copy_data => 0,
+        );
+        $self->tools([]) unless defined $self->tools;
+        push(@{$self->tools}, $modelfit);
+    }
+}
+
+sub modelfit_analyze
+{
+}
+
+sub _create_full_block
+{
+    my $self = shift;
+    my %parm = validated_hash(\@_,
+		input_model_name => { isa => 'Str' },
+		output_model_name => { isa => 'Str' },
+	);
+	my $input_model_name = $parm{'input_model_name'};
+	my $output_model_name = $parm{'output_model_name'};
+
+    my $base = model->new(filename => $input_model_name);
+    my $model = $base->copy(filename => "m1/$output_model_name");
+
+    my $omega_matrix = $model->problems->[0]->get_filled_omega_matrix(start_eta => 1);
+    my $size = @{$omega_matrix};
+    my @record_arr = ( "\$OMEGA BLOCK($size)" );
+    for (my $i = 0; $i < $size; $i++) {
+        my $row = "";
+        for (my $j = 0; $j <= $i; $j++) {
+            $row .= $omega_matrix->[$i]->[$j] . ' ';
+        }
+        push @record_arr, "$row\n";
+    }
+
+    my $omega = model::problem::omega->new(record_arr => \@record_arr);
+    $model->problems->[0]->omegas([ $omega ]);
+
+    $model->_write;
+
+    return $model;
 }
 
 
