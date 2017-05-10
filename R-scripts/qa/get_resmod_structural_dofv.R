7081fb3cf09e0430ed65b6695d93c385c0442334
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2017-05-10 13:31:50 +0200

    R-script:qa:Bugfix in Structural plots and dofv values

--- R-scripts/qa/get_resmod_structural_dofv.R
+++ R-scripts/qa/get_resmod_structural_dofv.R
@@ -2,7 +2,7 @@ get_resmod_structural_dofv <- function(directory, suffix) {
   resmod_file_exists <- get_resmod_table(directory, suffix)$resmod_file_exists
   if(resmod_file_exists) {
     resmod_structural_dofv <- get_resmod_table(directory, suffix)$resmod_table %>%
-      filter(model=="idv_varying_combined") %>%
+      filter(model=="idv_varying_theta") %>%
       select(dofv) %>%
       round(1) %>%
       as.character() # so we can see NA values
