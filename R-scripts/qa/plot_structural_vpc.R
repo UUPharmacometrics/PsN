b8cdc670e6d2d20e8c07c851c34ddeb1cc1fbe2d
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2017-05-23 10:55:12 +0200

    R-script:qa: NA in SCM if not run, bugfix in vpc plots

--- R-scripts/qa/plot_structural_vpc.R
+++ R-scripts/qa/plot_structural_vpc.R
@@ -1,4 +1,4 @@
-plot_structural_vpc <- function(obs,sim,shift_table,idv) {
+plot_structural_vpc <- function(obs,sim,shift_tab,idv) {
   sim_shifted <- sim %>%
     mutate(bin_index = findInterval(unlist(.[[toupper(idv)]]), shift_tab$bin_min),  # in which bin did observation go
            shift = shift_tab$shift[bin_index],
