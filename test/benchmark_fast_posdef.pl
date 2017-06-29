37de270c9d553f363c34a9a29da935d3304f1c5f
Gunnar Yngman <gunnar.yngman@pharmetheus.com>
2017-06-29 17:58:31 +0200

    better benchmark of fast posdef
    5x faster on 5x5 and 4000x on 50x50 (600x with block adjust)

--- test/benchmark_fast_posdef.pl
+++ test/benchmark_fast_posdef.pl
@@ -15,36 +15,49 @@ foreach my $adjust_blocks (0,1) {
 
     # settings: dimensionality and number of blocks to pass to check_blocks_posdef()
     # (larger matrix sizes give more relative speedup)
-    my $dim     = $adjust_blocks ? 20  : 20;
+    my @dims    = $adjust_blocks ? (5, 10, 20, 50) : (5, 10, 20, 50);
     my $nblocks = $adjust_blocks ? 100 : 1;
 
-    my $nelements = ($dim*$dim - $dim)/2 + $dim;
-    my $av = 0;
-    my $sd = 10;
-    my @xvec = random_normal($nblocks*$nelements, $av, $sd);
+    my $xvecs = [];
+    my $hash_arrs = [];
+    foreach my $dim (@dims) {
+        my $nelements = ($dim*$dim - $dim)/2 + $dim;
+        my $av = 0;
+        my $sd = 10;
+        my @xvec = random_normal($nblocks*$nelements, $av, $sd);
 
-    my $hash_arr = [];
-    for (my $i=0; $i<$nblocks; $i++) {
-        my @indices = (($i*$nelements)..($i*$nelements + $nelements - 1));
+        my $hash_arr = [];
+        for (my $i=0; $i<$nblocks; $i++) {
+            my @indices = (($i*$nelements)..($i*$nelements + $nelements - 1));
 
-        my $hash_ref = {'size' => $dim, 'indices' => \@indices};
-        push @{$hash_arr}, $hash_ref;
+            my $hash_ref = {'size' => $dim, 'indices' => \@indices};
+            push @{$hash_arr}, $hash_ref;
+        }
+
+        push @{$xvecs}, \@xvec;
+        push @{$hash_arrs}, $hash_arr;
     }
 
-    my @elapsed;
-    foreach my $fast_posdef (0,1) {
-        my $posdef_str = $fast_posdef ? "fast" : "slow";
-        printf "(%9s; %4s) n=%4u %2ux%2u random matrices", $adj_str, $posdef_str, $nblocks, $dim, $dim;
+    for (my $i=0; $i<scalar(@dims); $i++) {
+        my $dim = $dims[$i];
+        my @xvec = @{$xvecs->[$i]};
+        my $hash_arr = $hash_arrs->[$i];
 
-        my $start = [ gettimeofday() ];
-        my ($acc,$adj) = tool::sir::check_blocks_posdef(xvec => \@xvec,
-                                                        hash_array => $hash_arr,
-                                                        adjust_blocks => $adjust_blocks,
-                                                        fast_posdef_check => $fast_posdef);
-        my $elapsed = tv_interval($start);
-        print ", elapsed time: $elapsed s\n";
-        $elapsed[$fast_posdef] = $elapsed;
+        my @elapsed;
+        foreach my $fast_posdef (0,1) {
+            my $posdef_str = $fast_posdef ? "fast" : "slow";
+            printf "(%9s; %4s) n=%4u %2ux%2u random matrices", $adj_str, $posdef_str, $nblocks, $dim, $dim;
+
+            my $start = [ gettimeofday() ];
+            my ($acc,$adj) = tool::sir::check_blocks_posdef(xvec => \@xvec,
+                                                            hash_array => $hash_arr,
+                                                            adjust_blocks => $adjust_blocks,
+                                                            fast_posdef_check => $fast_posdef);
+            my $elapsed = tv_interval($start);
+            print ", elapsed time: $elapsed s\n";
+            $elapsed[$fast_posdef] = $elapsed;
+        }
+        my $speedup = $elapsed[0]/$elapsed[1];
+        printf "(speedup: %.2f)\n", $speedup;
     }
-    my $speedup = $elapsed[0]/$elapsed[1];
-    printf "(speedup: %.2f)\n", $speedup;
 }
