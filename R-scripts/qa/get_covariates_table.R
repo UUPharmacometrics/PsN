101d9b64b8ad8f773ea6b414b356d89564080466
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2017-08-17 10:54:42 +0200

    R-script: Round dofv values

--- R-scripts/qa/get_covariates_table.R
+++ R-scripts/qa/get_covariates_table.R
@@ -1,22 +1,26 @@
 get_covariates_table <- function(frem_table,scm_table,max_scm_table) {
   covariates_table <- rbind(c("FREM",frem_table[2:3]),max_scm_table,stringsAsFactors=F)
   if((scm_table$dOFV[1] == "ERROR" || scm_table$dOFV[1] == "NA") && length(scm_table$dOFV)==1) {
-    covariates_extra_table <- rbind(scm_table,c("FREM",format(frem_table$dOFV,digits=1,nsmall = 1)),stringsAsFactors=F)
+    if(frem_table$dOFV != "ERROR" && frem_table$dOFV != "NA") {
+      covariates_extra_table <- rbind(scm_table,c("FREM",format(round(as.numeric(frem_table$dOFV),2),digits=1,nsmall = 1)),stringsAsFactors=F)
+    } else {
+      covariates_extra_table <- rbind(scm_table,c("FREM",frem_table$dOFV),stringsAsFactors=F)
+    }
     colnames(covariates_extra_table) <- c("Covariate","dOFV")
   } else {
     sum_scm_dofv <- sum(as.numeric(scm_table$dOFV))
-    scm_table$Coef <- format(scm_table$Coef,trim=T,digits=1,nsmall=2,scientific=F)
+    scm_table$Coef <- format(round(scm_table$Coef,3),trim=T,digits=1,nsmall=2,scientific=F)
     covariates_extra_table <- rbind(scm_table,c("sum(SCMu)",sum_scm_dofv,rep("",ncol(scm_table)-2)),
                                     stringsAsFactors=F)
     colnames(covariates_extra_table) <- c("Covariate","dOFV","Coefficient")
     if(class(frem_table$dOFV)=="character") {
-      covariates_extra_table$dOFV <- format(as.numeric(covariates_extra_table$dOFV),trim=T,digits=1,nsmall=1,scientific=F)
+      covariates_extra_table$dOFV <- format(round(as.numeric(covariates_extra_table$dOFV),2),trim=T,digits=1,nsmall=1,scientific=F)
       covariates_extra_table <- rbind(covariates_extra_table,c("FREM",frem_table$dOFV,rep("",ncol(scm_table)-2)),
                                       stringsAsFactors=F)
     } else {
       covariates_extra_table <- rbind(covariates_extra_table,c("FREM",frem_table$dOFV,rep("",ncol(scm_table)-2)),
                                       stringsAsFactors=F)
-      covariates_extra_table$dOFV <- format(as.numeric(covariates_extra_table$dOFV),trim=T,digits=1,nsmall=1,scientific=F)
+      covariates_extra_table$dOFV <- format(round(as.numeric(covariates_extra_table$dOFV),2),trim=T,digits=1,nsmall=1,scientific=F)
     }
 
   }
