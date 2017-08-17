2640a90893c786b6523b83accf9b5accfb6cff5f
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2017-08-17 10:54:34 +0200

    qa: Fixes for updates in xpose and purrr

--- R-scripts/qa/get_table_for_vpc.R
+++ R-scripts/qa/get_table_for_vpc.R
@@ -5,8 +5,8 @@ get_tables_for_vpc <- function(obs_table,obs_extra_table,sim_table,sim_extra_tab
   }
   
   if(make_vpc) {
-    obs <- as.data.frame(read_nm_tables(obs_table))
-    extra_obs <- read_nm_tables(obs_extra_table)
+    obs <- as.data.frame(xpose::read_nm_tables(obs_table))
+    extra_obs <- xpose::read_nm_tables(obs_extra_table)
     
     #choose DVID
     if(dvid!='NA') {
@@ -40,10 +40,10 @@ get_tables_for_vpc <- function(obs_table,obs_extra_table,sim_table,sim_extra_tab
       }
     }
     add_cols <- obs[,c(idv_all)]
-    sim <- read_nm_tables(sim_table)
+    sim <- xpose::read_nm_tables(sim_table)
     if(dvid != "NA") {
       if(all(colnames(sim)!=dvid_name)) {
-        sim_extra <- read_nm_tables(sim_extra_table)
+        sim_extra <- xpose::read_nm_tables(sim_extra_table)
         dvid_column_nr <- which(colnames(sim_extra)== dvid_name)
         dvid_sim_col <- sim_extra[,dvid_column_nr]
         sim <- cbind(sim,dvid_sim_col)
--- R-scripts/qa/individual_plots.R
+++ R-scripts/qa/individual_plots.R
@@ -1,7 +1,7 @@
 individual_plots <- function(file_name,ID_nr) {
   if(file.exists(file_name) && (length(ID_nr)!=0) ) {
     p <- xpose_data(file=file_name) %>% 
-      set_vars_type(ipred="OPRED") %>% 
+      set_var_types(ipred="OPRED") %>% 
       filter(ID %in% !!ID_nr) %>% 
       ind_plots(nrow=4,ncol=3,title="Individual plots",subtitle="",caption="") + theme(legend.title=element_blank()) + theme_bw()
     return(p)
