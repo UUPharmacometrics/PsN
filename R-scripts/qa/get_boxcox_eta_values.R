e7a4f512aa84e31cc4d83c715519ef151797e7ca
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2017-08-21 15:23:08 +0200

    qa:get boxcox eta values and tdist eta values in one func

--- R-scripts/qa/get_boxcox_eta_values.R
+++ R-scripts/qa/get_boxcox_eta_values.R
@@ -1,7 +1,11 @@
-get_boxcox_eta_values <- function(working.directory) {
-  if(file.exists(file.path(working.directory,"boxcox.phi"))) {
-    boxcox_phi_table <- read.table(file.path(working.directory,"boxcox.phi"),skip=1,header=T,stringsAsFactors = F)
-    eta_table <- boxcox_phi_table %>% select(grep("ETA",colnames(.))) %>% gather(ETA_name) %>% 
+get_eta_values <- function(working.directory,param_model) {
+  
+  #param_model should be "boxcox" or "tdist" only!
+  
+  phi_file_path <- file.path(working.directory,paste0(param_model,".phi"))
+  if(file.exists(phi_file_path)) {
+    phi_table <- read.table(phi_file_path,skip=1,header=T,stringsAsFactors = F)
+    eta_table <- phi_table %>% select(grep("ETA",colnames(.))) %>% gather(ETA_name) %>% 
       mutate(ETA_name=sub("\\.","(",.$ETA_name)) %>% mutate(ETA_name=sub("[.]$",")",.$ETA_name)) 
     } else {
     eta_table <- data.frame()
