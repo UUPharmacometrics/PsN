5fe4685e9a4efb8b9257e6b89ad405409ce7c8b8
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2017-09-05 09:52:23 +0200

    output: Remove unused function raw_seomegas

--- lib/output.pm
+++ lib/output.pm
@@ -2435,26 +2435,6 @@ sub correlation_matrix
 	return \@correlation_matrix;
 }
 
-sub raw_seomegas
-{
-	my $self = shift;
-	my %parm = validated_hash(\@_,
-							  problems => { isa => 'ArrayRef[Int]', optional => 1 },
-							  subproblems => { isa => 'ArrayRef[Int]', optional => 1 }
-		);
-	my @problems = defined $parm{'problems'} ? @{$parm{'problems'}} : ();
-	my @subproblems = defined $parm{'subproblems'} ? @{$parm{'subproblems'}} : ();
-	my @raw_seomegas = @{$self->access_any(attribute=>'raw_seomegas',problems=>\@problems,subproblems=>\@subproblems)};
-
-
-    # raw_seomegas returns the (raw) omega standard error estimates.
-    # See L</comegas> for details of the method arguments.
-    #
-    # Level:  Sub problem
-    croak("This function cannot be used, is only a placeholder. Rewrite program.");
-	return \@raw_seomegas;
-}
-
 sub raw_sesigmas
 {
 	my $self = shift;
