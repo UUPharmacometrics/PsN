0153a7fc3e48fae7617fb2f968586563fb85b94c
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2017-09-28 17:20:17 +0200

    common: rewrite R info in meta.yaml if needed

--- R-scripts/common/R_info.R
+++ R-scripts/common/R_info.R
@@ -1,7 +1,7 @@
 R_info <- function(directory) {
   #get R version
   if(file.exists(file.path(directory,"meta.yaml"))) {
-    yaml_file <- yaml.load_file(file.path(directory,"meta.yaml"))
+    yaml_file <- yaml::yaml.load_file(file.path(directory,"meta.yaml"))
     if(!exists("R_version",yaml_file)) {
       cat(yaml::as.yaml(list(R_version=strsplit(devtools::session_info()[[1]]$version," ")[[1]][3],
                              R_system=devtools::session_info()[[1]]$system,
@@ -10,6 +10,26 @@ R_info <- function(directory) {
                         indent.mapping.sequence = TRUE),
           file=file.path(directory,"meta.yaml"),
           append = TRUE)
+    } else {
+      yaml_file <- yaml::yaml.load_file(file.path(directory,"meta.yaml"))
+      yaml_file$R_packages <- NULL
+      yaml_file$R_system <- NULL
+      yaml_file$R_version <- NULL
+      yaml_file$R_version <- strsplit(devtools::session_info()[[1]]$version," ")[[1]][3]
+      yaml_file$R_system <- devtools::session_info()[[1]]$system
+      yaml_file$R_packages <- as.data.frame(devtools::session_info()[[2]])[,c("package","version","source")]
+      
+      # sort
+      tag_names <-  sort(names(yaml_file))
+      new_yaml_file <- list()
+      for(i in 1:length(tag_names)) {
+        new_yaml_file[[tag_names[i]]] <- yaml_file[[tag_names[i]]]
+      }
+      #write in the file
+      cat(yaml::as.yaml(new_yaml_file,
+                        column.major = F,
+                        indent.mapping.sequence = TRUE),
+          file=file.path(directory,"meta.yaml"))
     }
   } else {
     cat(yaml::as.yaml(list(R_version=strsplit(devtools::session_info()[[1]]$version," ")[[1]][3],
