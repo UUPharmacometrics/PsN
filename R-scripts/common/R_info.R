355fb9b739a3ea83f0e470c4b329df52c8575300
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2017-10-03 08:47:25 +0200

    common: reorganize R package information in yaml file

--- R-scripts/common/R_info.R
+++ R-scripts/common/R_info.R
@@ -1,11 +1,19 @@
 R_info <- function(directory) {
-  #get R version
+  #get loaded R packages
+  R_packages <- as.data.frame(devtools::session_info()[[2]])[,c("package","version","source")]
+  R_packages_vec <- paste0(R_packages$package[1],"-",R_packages$version[1]," from ",R_packages$source[1])
+  for(i in 2:nrow(R_packages)) {
+    R_packages_vec <- c(R_packages_vec,paste0(R_packages$package[i],"-",R_packages$version[i]," from ",R_packages$source[i]))
+  }
+  R_packages <- R_packages_vec
+
+  #write in to the yaml file
   if(file.exists(file.path(directory,"meta.yaml"))) {
     yaml_file <- yaml::yaml.load_file(file.path(directory,"meta.yaml"))
     if(!exists("R_version",yaml_file)) {
       cat(yaml::as.yaml(list(R_version=strsplit(devtools::session_info()[[1]]$version," ")[[1]][3],
                              R_system=devtools::session_info()[[1]]$system,
-                             R_packages=as.data.frame(devtools::session_info()[[2]])[,c("package","version","source")]),
+                             R_packages=R_packages),
                         column.major = F,
                         indent.mapping.sequence = TRUE),
           file=file.path(directory,"meta.yaml"),
@@ -17,8 +25,8 @@ R_info <- function(directory) {
       yaml_file$R_version <- NULL
       yaml_file$R_version <- strsplit(devtools::session_info()[[1]]$version," ")[[1]][3]
       yaml_file$R_system <- devtools::session_info()[[1]]$system
-      yaml_file$R_packages <- as.data.frame(devtools::session_info()[[2]])[,c("package","version","source")]
-      
+      yaml_file$R_packages <- R_packages
+
       # sort
       tag_names <-  sort(names(yaml_file))
       new_yaml_file <- list()
@@ -34,7 +42,7 @@ R_info <- function(directory) {
   } else {
     cat(yaml::as.yaml(list(R_version=strsplit(devtools::session_info()[[1]]$version," ")[[1]][3],
                            R_system=devtools::session_info()[[1]]$system,
-                           R_packages=as.data.frame(devtools::session_info()[[2]])[,c("package","version","source")]),
+                           R_packages=R_packages),
                       column.major = F,
                       indent.mapping.sequence = TRUE),
         file=file.path(directory,"meta.yaml"))
