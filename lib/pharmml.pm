97f202d532b17f0532b9c1e8d74ec06d2d4e066d
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2017-10-06 10:20:49 +0200

    nmoutput2so: Fix random variables and add correlations to minimal pharmml

--- lib/pharmml.pm
+++ lib/pharmml.pm
@@ -143,10 +143,24 @@ END
 
     print_population_parameters(file => $file, model => $model);
     print_random_variables(file => $file, model => $model);
+    print_correlations(file => $file, model => $model);
 
 print $file <<'END';
         </ParameterModel>
 END
+}
+
+sub get_name
+{
+    my $option = shift;
+    my $name = $option->label;
+    if (not defined $name) {
+        $name = $option->coordinate_string;
+    }
+    if (not so::xml::match_symbol_idtype($name)) {
+        $name = so::xml::mangle_symbol_idtype($name);
+    }
+    return $name;
 }
 
 sub print_population_parameters
@@ -160,14 +174,7 @@ sub print_population_parameters
 
     for my $record (@{$model->problems->[0]->thetas}, @{$model->problems->[0]->omegas}, @{$model->problems->[0]->sigmas}) {
         for my $option (@{$record->options}) {
-            my $name = $option->label;
-            if (not defined $name) {
-                $name = $option->coordinate_string;
-            }
-            if (not so::xml::match_symbol_idtype($name)) {
-                $name = so::xml::mangle_symbol_idtype($name);
-            }
-
+            my $name = get_name($option);
             print $file '            <PopulationParameter symbId="' . $name . '"/>' . "\n"; # FIXME if no label and symbol washing
         }
     }
@@ -186,15 +193,19 @@ sub print_random_variables
     for my $record (@{$model->problems->[0]->omegas}, @{$model->problems->[0]->sigmas}) {
         for my $option (@{$record->options}) {
             if ($option->on_diagonal) {
-                my $variance = $option->init; 
+                my $symbol = get_name($option);
                 my $name = "ETA";
+                my $var_blkId = "vm_eta";
+                my $var_symbId = "ID";
                 if ($option->coordinate_string =~ /SIGMA/) {
                     $n = 1;
                     $name = "EPS";
+                    $var_blkId = "vm_err";
+                    $var_symbId = "DV";
                 }
                 print $file ' ' x 12, "<RandomVariable symbId=\"$name$n\">\n";
                 print $file ' ' x 16, "<ct:VariabilityReference>\n";
-                print $file ' ' x 20, "<ct:SymbRef blkIdRef=\"vm_eta\" symbIdRef=\"ID\"/>\n";
+                print $file ' ' x 20, "<ct:SymbRef blkIdRef=\"$var_blkId\" symbIdRef=\"$var_symbId\"/>\n";
                 print $file ' ' x 16, "</ct:VariabilityReference>\n";
                 print $file ' ' x 16, "<Distribution>\n";
                 print $file ' ' x 20, "<ProbOnto xmlns=\"http://www.pharmml.org/probonto/ProbOnto\" name=\"Normal2\">\n";   # FIXME: Does not support SD
@@ -205,13 +216,75 @@ sub print_random_variables
                 print $file ' ' x 24, "</Parameter>\n";
                 print $file ' ' x 24, "<Parameter name=\"var\">\n";
                 print $file ' ' x 28, "<ct:Assign>\n";
-                print $file ' ' x 32, "<ct:Real>$variance</ct:Real>\n";
+                print $file ' ' x 32, "<ct:SymbRef symbIdRef=\"$symbol\"/>\n";
                 print $file ' ' x 28, "</ct:Assign>\n";
                 print $file ' ' x 24, "</Parameter>\n";
                 print $file ' ' x 20, "</ProbOnto>\n";
                 print $file ' ' x 16, "</Distribution>\n";
                 print $file ' ' x 12, "</RandomVariable>\n";
                 $n++;
+            }
+        }
+    }
+}
+
+sub print_correlations
+{
+    my %parm = validated_hash(\@_,
+        file => { isa => 'Ref' },
+        model => { isa => 'model' },
+    );
+    my $file = $parm{'file'};
+    my $model = $parm{'model'};
+
+    my %diag;       # Coordstring to symbol
+
+    for my $record (@{$model->problems->[0]->omegas}, @{$model->problems->[0]->sigmas}) {
+        for my $option (@{$record->options}) {
+            if ($option->on_diagonal) {
+                my $symbol = get_name($option);
+                $diag{$option->coordinate_string} = $symbol;
+            }
+        }
+    }
+
+    for my $record (@{$model->problems->[0]->omegas}, @{$model->problems->[0]->sigmas}) {
+        for my $option (@{$record->options}) {
+            if (not $option->on_diagonal) {
+                my $symbol = get_name($option);
+                $option->coordinate_string =~ /(\d+),(\d+)/;
+                my $first = $1;
+                my $second = $2;
+                $option->coordinate_string =~ /(\w+)/;
+                my $type = $1;
+                my $first_diag = "$type($first,$first)"; 
+                my $second_diag = "$type($second,$second)";
+                my $first_diag_name = $diag{$first_diag};
+                my $second_diag_name = $diag{$second_diag};
+                my $var_blkId = "vm_eta";
+                my $var_symbId = "ID";
+                if ($option->coordinate_string =~ /SIGMA/) {
+                    $var_blkId = "vm_err";
+                    $var_symbId = "DV";
+                }
+                print $file ' ' x 12, "<Correlation>\n";
+                print $file ' ' x 16, "<ct:VariabilityReference>\n";
+                print $file ' ' x 20, "<ct:SymbRef blkIdRef=\"$var_blkId\" symbIdRef=\"$var_symbId\"/>\n";
+                print $file ' ' x 16, "</ct:VariabilityReference>\n";
+                print $file ' ' x 16, "<Pairwise>\n";
+                print $file ' ' x 20, "<RandomVariable1>\n";
+                print $file ' ' x 24, "<ct:SymbRef symbIdRef=\"$first_diag_name\"/>\n";
+                print $file ' ' x 20, "</RandomVariable1>\n";
+                print $file ' ' x 20, "<RandomVariable2>\n";
+                print $file ' ' x 24, "<ct:SymbRef symbIdRef=\"$second_diag_name\"/>\n";
+                print $file ' ' x 20, "</RandomVariable2>\n";
+                print $file ' ' x 20, "<CorrelationCoefficient>\n";
+                print $file ' ' x 24, "<ct:Assign>\n";
+                print $file ' ' x 28, "<ct:SymbRef symbIdRef=\"$symbol\"/>\n";
+                print $file ' ' x 24, "</ct:Assign>\n";
+                print $file ' ' x 20, "</CorrelationCoefficient>\n";
+                print $file ' ' x 16, "</Pairwise>\n";
+                print $file ' ' x 12, "</Correlation>\n";
             }
         }
     }
