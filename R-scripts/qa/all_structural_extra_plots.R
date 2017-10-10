c96c29ab85b95b0f24fe39c2616211a8af8e1433
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2017-10-10 14:40:44 +0200

    qa: Make subtitles work on html

--- R-scripts/qa/all_structural_extra_plots.R
+++ R-scripts/qa/all_structural_extra_plots.R
@@ -24,7 +24,7 @@ all_structural_extra_plots <- function(working.directory,model.filename,resmod_s
       vpc_captions <- all_captions$idv_vpc_captions
       
       #print
-      cat(paste0("##",resmod_structural_details[[i]]$idv_text))
+      cat(paste0("##",resmod_structural_details[[i]]$idv_text,"\n\n"))
       first_table <- keep_symbols(resmod_structural_details[[i]]$first_table,type)
       first_table <- ztable_sub(first_table,type=type,include.colnames = F,include.rownames = F,longtable = T,align="lr")
       print(first_table)
