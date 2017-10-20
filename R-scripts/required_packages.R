223dff418b0776dc3c51f8af2faa8a6e98bc124b
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2017-10-20 15:47:26 +0200

    If library path not set, install libraries as usually

--- R-scripts/required_packages.R
+++ R-scripts/required_packages.R
@@ -1,15 +1,16 @@
 # input arguments from command line
 args <- commandArgs(TRUE)
 
-if(length(args) < 1) {
-  message("Error: Please specify path where to install R libraries! R library installation will not continue!")
-  quit()
-}
-path <- as.character(args[1])
-#check if path exists
-if(is.na(file.info(path)$isdir)) {
-  message(paste0("Error: Given path ",path," does not exist! R library installation will not continue!"))
-  quit()
+if(length(args) == 1) {
+  path <- as.character(args[1])
+  #check if path exists
+  if(is.na(file.info(path)$isdir)) {
+    message(paste0("Error: Given path ",path," does not exist! R library installation will not continue!"))
+    quit()
+  }
+  
+  # set path where to install libraries
+  .libPaths(path)
 }
 
 local({
@@ -17,32 +18,31 @@ local({
   r["CRAN"] <- "https://cran.r-project.org"
   options(repos = r)
 })
-
-# set path where to install libraries
-.libPaths(path)
+path <- .libPaths()[1]
+message(paste0("Installing R libraries in ",path,"."))
 # install packages
-install.packages("caTools", dependencies=TRUE)
-install.packages("expm", dependencies=TRUE)
-install.packages("gplots", dependencies=TRUE)
-install.packages("grid", dependencies=TRUE)
-install.packages("gridExtra", dependencies=TRUE)
-install.packages("MASS", dependencies=TRUE)
-install.packages("MCMCpack", dependencies=TRUE)
-install.packages("rmarkdown",dependencies = TRUE)
-install.packages("ggthemes", dependencies=TRUE)
-install.packages("PEIP", dependencies=TRUE)
-install.packages("PerformanceAnalytics", dependencies=TRUE)
-install.packages("plotrix", dependencies=TRUE)
-install.packages("plyr", dependencies=TRUE)
-install.packages("RColorBrewer", dependencies=TRUE)
-install.packages("reshape", dependencies=TRUE)
-install.packages("reshape2", dependencies=TRUE)
-install.packages("scales", dependencies=TRUE)
-install.packages("stats4", dependencies=TRUE)
-install.packages("tidyverse", dependencies=TRUE)
-install.packages("xpose4", dependencies=TRUE)
-install.packages("ztable", dependencies=TRUE)
-install.packages("devtools", dependencies=TRUE)
-install.packages("yaml", dependencies=TRUE)
+install.packages("caTools", dependencies=TRUE,lib = path)
+install.packages("expm", dependencies=TRUE,lib = path)
+install.packages("gplots", dependencies=TRUE,lib = path)
+install.packages("grid", dependencies=TRUE,lib = path)
+install.packages("gridExtra", dependencies=TRUE,lib = path)
+install.packages("MASS", dependencies=TRUE,lib = path)
+install.packages("MCMCpack", dependencies=TRUE,lib = path)
+install.packages("rmarkdown",dependencies = TRUE,lib = path)
+install.packages("ggthemes", dependencies=TRUE,lib = path)
+install.packages("PEIP", dependencies=TRUE,lib = path)
+install.packages("PerformanceAnalytics", dependencies=TRUE,lib = path)
+install.packages("plotrix", dependencies=TRUE,lib = path)
+install.packages("plyr", dependencies=TRUE,lib = path)
+install.packages("RColorBrewer", dependencies=TRUE,lib = path)
+install.packages("reshape", dependencies=TRUE,lib = path)
+install.packages("reshape2", dependencies=TRUE,lib = path)
+install.packages("scales", dependencies=TRUE,lib = path)
+install.packages("stats4", dependencies=TRUE,lib = path)
+install.packages("tidyverse", dependencies=TRUE,lib = path)
+install.packages("xpose4", dependencies=TRUE,lib = path)
+install.packages("ztable", dependencies=TRUE,lib = path)
+install.packages("devtools", dependencies=TRUE,lib = path)
+install.packages("yaml", dependencies=TRUE,lib = path)
 devtools::install_github("UUPharmacometrics/xpose")
 devtools::install_github("ronkeizer/vpc")
