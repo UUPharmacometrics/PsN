6e21c381266106facdcecc135ff367bf6dd985a9
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2018-02-08 14:02:53 +0100

    qa: Delete not used parameter idv_name

--- R-scripts/qa/resmod_structural_details_tables.R
+++ R-scripts/qa/resmod_structural_details_tables.R
@@ -1,4 +1,4 @@
-resmod_structural_details_tables <- function(working.directory,base_dataset,original_max0_model,CWRES_table,idv_all,idv_name,dvid_name,nonlinear,quiet=F) {
+resmod_structural_details_tables <- function(working.directory,base_dataset,original_max0_model,CWRES_table,idv_all,dvid_name,nonlinear,quiet=F) {
   resmod_structural_details_list <- list()
   if(length(idv_all)!=0) {
     #check if dvid exist
@@ -33,7 +33,7 @@ resmod_structural_details_tables <- function(working.directory,base_dataset,orig
           nonlinear==FALSE) {
             
           table = get_resmod_structural_details(directory=working.directory, suffix = idv, dvid=dvid_nr_idv[[i]][j]) %>%
-            .calc_and_add_shift_from_cwres(orig_ext_file,base_dataset,CWRES_table,idv,idv_name, dvid=dvid_nr_idv[[i]][j],dvid_name)
+            .calc_and_add_shift_from_cwres(orig_ext_file,base_dataset,CWRES_table,idv,dvid=dvid_nr_idv[[i]][j],dvid_name)
             
           second_table = data.frame(C1=paste0(format(table$bin_min,nsmall=2),"  :  ",format(table$bin_max,nsmall=2)),C2=as.character(format(round(table$value,2),nsmall=2)),
                                     stringsAsFactors = F)
