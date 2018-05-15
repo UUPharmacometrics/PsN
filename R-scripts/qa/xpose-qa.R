3f5116403b0a3fefffa956c1367aaade8eadf1ef
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2018-05-15 14:56:45 +0200

    QA: replace solve function to MASS::ginv

--- R-scripts/qa/xpose-qa.R
+++ R-scripts/qa/xpose-qa.R
@@ -202,7 +202,7 @@ var_given_eta <- function(df_deta, omega, eta_index){
 }
 
 schur_complement <- function(M, block_index){
-  solve(M)[-block_index, -block_index] %>% solve()
+  MASS::ginv(M)[-block_index, -block_index] %>% MASS::ginv()
   #  M[-block_index, -block_index] - M[block_index, -block_index] %*% MASS::ginv(M[block_index, block_index]) %*% M[-block_index, block_index]
 }
 
