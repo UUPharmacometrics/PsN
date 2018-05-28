82bb134be149fee78fd6bbb3951d27bc85aec979
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2018-05-28 10:15:49 +0200

    qa: Replace individual plot OPRED label name to IPRED

--- R-scripts/qa/individual_plots.R
+++ R-scripts/qa/individual_plots.R
@@ -1,7 +1,8 @@
 individual_plots <- function(file_name,ID_nr,quiet=F) {
   if(file.exists(file_name) && (length(ID_nr)!=0) ) {
     p <- xpose::xpose_data(file=file_name) %>%
-      xpose::set_var_types(ipred="OPRED") %>% 
+      xpose::mutate(IPRED=OPRED) %>% 
+      xpose::set_var_types(ipred="IPRED") %>% 
       dplyr::filter(ID %in% !!ID_nr) %>%
       xpose::ind_plots(nrow=4, ncol=3, title="Individual plots",subtitle="",caption="") + labs(color = NULL, linetype = NULL, alpha = NULL) + theme_bw()
     return(p)
