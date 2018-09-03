52cd483220c9c12d542e7eba3fda33317e9d07f4
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2018-09-03 15:26:17 +0200

    qa: Add DVID

--- R-scripts/qa/results_summary.R
+++ R-scripts/qa/results_summary.R
@@ -11,7 +11,8 @@ yaml_summary <- function(ofv_table,
                          resmod_table_list,
                          ii_list,
                          outlier_table_list,
-                         overview_table_list
+                         overview_table_list,
+                         dvid_name
                          ) {
   #function to check varables existance
   check_argument_existance <- function(x) {
@@ -30,7 +31,7 @@ yaml_summary <- function(ofv_table,
   add_etas_list <- check_argument_existance(add_etas_list)
   iov_list <- check_argument_existance(iov_list)
   
- #add nonlinear and linearized ofv values to the tree
+  #add nonlinear and linearized ofv values to the tree
   tree <- list(
     nonlinear_base_model_ofv = ofv_table[1,2],
     linearized_base_model_ofv_before_estimation = ofv_table[2,2],
@@ -38,24 +39,126 @@ yaml_summary <- function(ofv_table,
     sum_of_individual_ofv_values = ofv_table[4,2]
   )
   
-  # overview table
-  ovw_table <- overview_table_list$overview_table
-  colnames(ovw_table)[1] <- "name"
-  # group_name <- rep(overview_table_list$rgroup_names,times = overview_table_list$n.rgroup)
-  ovw_table <- ovw_table %>%
-    mutate(group_name=rep(overview_table_list$rgroup_names,times = overview_table_list$n.rgroup),
-           model_name=paste0(group_name," (",name,")"))
-  #add overview table to the tree
-  tree$dofv <- list()
-  tree$additional_param <- list()
-  for(i in 1:nrow(ovw_table)) {
-    tree$dofv[[ovw_table$model_name[i]]] <- ovw_table$dOFV[i]
-    tree$additional_param[[ovw_table$model_name[i]]] <- ovw_table$`Additional parameters`[i]
+ #check if dvid exists
+  if(dvid_name!='') {
+    tree$dvid_exists <- TRUE
+    tree$dvid_name <- dvid_name
+  } else {
+    tree$dvid_exists <- FALSE
   }
   
-  #strunctural tables
-  ######################   ckeck if multiple dvids  ###############################
-  tree$structural <- list()
+  #overview table
+  ovw_table <- overview_table_list$overview_table
+  colnames(ovw_table)[1] <- "name"
+  group_name <- rep(overview_table_list$rgroup_names,times = overview_table_list$n.rgroup)
+  ovw_table <- ovw_table %>%
+    mutate(group_name=rep(overview_table_list$rgroup_names,times = overview_table_list$n.rgroup))
+  
+  # overview (structural)
+  tree$overview <- list()
+  overview <- ovw_table %>%
+    filter(group_name=="Structural Model") 
+  if(tree$dvid_exists==TRUE) {
+    overview <- overview %>%
+      mutate(dvid=case_when(grepl(dvid_name,name) ~ name,
+                            TRUE ~ as.character(NA)),
+             dvid=as.numeric(gsub("\\D", "", dvid))) %>%
+      fill(dvid, .direction = "down") %>%
+      filter(!(grepl(dvid_name,name)))
+    
+    tree$overview$structural <- list()
+    for(i in 1:length(unique(overview$dvid))) {
+      tree$overview$structural[[i]] <- list()
+      tree$overview$structural[[i]]$dvid_value <- unique(overview$dvid)[i]
+      overview_dvid <- overview %>%
+        filter(dvid==unique(overview$dvid)[i])
+
+      for(j in 1:nrow(overview_dvid)) {
+        tree$overview$structural[[i]]$dofv[[overview$name[j]]] <- overview_dvid$dOFV[j]
+        tree$overview$structural[[i]]$additional_param[[overview$name[j]]] <- overview_dvid$`Additional parameters`[j]
+      }
+    }
+  } else {
+    tree$overview$structural[[1]] <- list()
+    for(i in 1:nrow(overview)) {
+      tree$overview$structural[[1]]$dofv[[overview$name[i]]] <- overview$dOFV[i]
+      tree$overview$structural[[1]]$additional_param[[overview$name[i]]] <- overview$`Additional parameters`[i]
+    }
+  }
+  
+  # overview (parameter variability)
+  tree$overview$param_variability <- list()
+  par_variab <- ovw_table %>%
+    filter(group_name=="Parameter Variability Model")
+  for(i in 1:nrow(par_variab)) {
+    tree$overview$param_variability$dofv[[par_variab$name[i]]] <- par_variab$dOFV[i]
+    tree$overview$param_variability$additional_param[[par_variab$name[i]]] <- par_variab$`Additional parameters`[i]
+  }
+  
+  # overview (frem)
+  tree$overview$frem <- list()
+  frem_part <- ovw_table %>%
+    filter(group_name=="Covariates") %>%
+    slice(1)
+  tree$overview$frem$dofv[[frem_part$name]] <- frem_part$dOFV
+  tree$overview$frem$additional_param[[frem_part$name]] <- frem_part$`Additional parameters`
+  
+  # overview (scm)
+  tree$overview$scm <- list()
+  scm_part <- ovw_table %>%
+    filter(group_name=="Covariates") %>%
+    slice(2)
+  tree$overview$scm$dofv[[scm_part$name]] <- scm_part$dOFV
+  tree$overview$scm$additional_param[[scm_part$name]] <- scm_part$`Additional parameters`
+  
+  # overview (ruv)
+  overview <- ovw_table %>%
+    filter(group_name=="Residual Error Model") 
+  if(tree$dvid_exists==TRUE) {
+    overview <- overview %>%
+      mutate(dvid=case_when(grepl(dvid_name,name) ~ name,
+                            TRUE ~ as.character(NA)),
+             dvid=as.numeric(gsub("\\D", "", dvid))) %>%
+      fill(dvid, .direction = "down") %>%
+      filter(!(grepl(dvid_name,name)))
+    
+    tree$overview$residual_error <- list()
+    for(i in 1:length(unique(overview$dvid))) {
+      tree$overview$residual_error[[i]] <- list()
+      tree$overview$residual_error[[i]]$dvid_value <- unique(overview$dvid)[i]
+      overview_dvid <- overview %>%
+        filter(dvid==unique(overview$dvid)[i])
+      
+      for(j in 1:nrow(overview_dvid)) {
+        tree$overview$residual_error[[i]]$dofv[[overview$name[j]]] <- overview_dvid$dOFV[j]
+        tree$overview$residual_error[[i]]$additional_param[[overview$name[j]]] <- overview_dvid$`Additional parameters`[j]
+      }
+    }
+  } else {
+    tree$overview$residual_error[[1]] <- list()
+    for(i in 1:nrow(overview)) {
+      tree$overview$residual_error[[1]]$dofv[[overview$name[i]]] <- overview$dOFV[i]
+      tree$overview$residual_error[[1]]$additional_param[[overview$name[i]]] <- overview$`Additional parameters`[i]
+    }
+  }
+  
+  # overview (cdd)
+  tree$overview$infl_id <- list()
+  cdd_part <- ovw_table %>%
+    filter(group_name=="Influential Individuals") %>%
+    slice(1)
+  tree$overview$infl_id$dofv[[cdd_part$name]] <- cdd_part$dOFV
+  tree$overview$infl_id$additional_param[[cdd_part$name]] <- cdd_part$`Additional parameters`
+  
+  # overview (simeval)
+  tree$overview$outliers <- list()
+  simeval_part <- ovw_table %>%
+    filter(group_name=="Outliers") %>%
+    slice(1)
+  tree$overview$outliers$dofv[[simeval_part$name]] <- simeval_part$dOFV
+  tree$overview$outliers$additional_param[[simeval_part$name]] <- simeval_part$`Additional parameters`
+  
+  #structural part
   if(!is.null(resmod_structural_details)) {
     for(i in 1:length(resmod_structural_details)) {
       resmod_structural_details_modified <- resmod_structural_details[[i]]
@@ -64,7 +167,7 @@ yaml_summary <- function(ofv_table,
       resmod_structural_details_modified$first_table <- NULL
       tree$structural[[i]] <- resmod_structural_details_modified
     }
-  }
+  } 
   
   #parameter variability tables
   tree$parameter_variability <- list()
@@ -92,17 +195,14 @@ yaml_summary <- function(ofv_table,
   
   #resmod tables
   tree$resmod <- list()
-  if(length(resmod_table_list$dvid_nr==1) & resmod_table_list$dvid_nr=="NA") {
-    tree$resmod$dvid_values <- "No DVIDs"
-  } else {
-    tree$resmod$dvid_values <- resmod_table_list$dvid_nr
-  }
-  if(tree$resmod$dvid_values=="No DVIDs") {
-    tree$resmod[[paste0("resmod_table_no_dvids")]] <- resmod_table_list$resmod_ruv_table_list[[1]]
-  } else {
+  if(tree$dvid_exists==TRUE) {
     for(i in 1:length(resmod_table_list$dvid_nr)) {
-      tree$resmod[[paste0("remosd_table_with_DVID_",i)]] <- resmod_table_list$resmod_ruv_table_list[[i]]
+      tree$resmod[[i]] <- list()
+      tree$resmod[[i]]$dvid_value <- resmod_table_list$dvid_nr[i]
+      tree$resmod[[i]]$resmod_ruv_table  <- resmod_table_list$resmod_ruv_table_list[[i]]
     }
+  } else {
+    tree$resmod[[1]]$resmod_ruv_table  <- resmod_table_list$resmod_ruv_table_list
   }
   
   #influential id tables
