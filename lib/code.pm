0da011b633bb48cc310a78c4610a5891e4873b56
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2018-11-26 09:32:01 +0100

    New code function to add abbreviated code

--- lib/code.pm
+++ lib/code.pm
@@ -6,6 +6,54 @@ use strict;
 use warnings;
 use include_modules;
 use MooseX::Params::Validate;
+use model;
+
+sub append_verbatim_code
+{
+    # Append verbatim code at verbatim section specified (LAST or FIRST)
+	my %parm = validated_hash(\@_,
+        model => { isa => 'model' },
+        code_record => { isa => 'Str' },
+        pos => { isa => 'Str' },
+        code => { isa => 'ArrayRef[Str]' },
+    );
+    my $model = $parm{'model'};
+    my $code_record = $parm{'code_record'};
+    my $pos = $parm{'pos'};
+    my $code = $parm{'code'};
+
+    if (not ($pos eq 'LAST' or $pos eq 'FIRST')) {
+        croak("pos must be one of LAST or FIRST");
+    }
+
+    my $code_array_name = $code_record . 's';
+    my $code_array = $model->problems->[0]->$code_array_name;
+
+    my $verbatim_code;
+    my $record;
+    if ($pos eq 'FIRST') {
+        $record = $code_array->[0];
+        $verbatim_code = $record->verbatim_first;
+    } else {
+        $record = $code_array->[scalar(@$code_record) - 1];
+        $verbatim_code = $record->verbatim_last;
+    }
+
+    if (not defined $verbatim_code) {
+        $verbatim_code = [];
+    }
+
+    for my $row (@$code) {
+        push @$verbatim_code, '"' . $row;
+    }
+
+    if ($pos eq 'FIRST') {
+        $record->verbatim_first($verbatim_code);
+    } else {
+        $record->verbatim_last($verbatim_code);
+    }
+}
+
 
 sub generate_sum
 {
--- test/unit/code.t
+++ test/unit/code.t
@@ -8,8 +8,21 @@ use FindBin qw($Bin);
 use lib "$Bin/.."; #location of includes.pm
 use includes; #file with paths to PsN packages
 
+use model;
 use code;
 
+# append_verbatim_code
+
+my $modeldir = $includes::testfiledir;
+my $model = model->new(filename => "$modeldir/pheno.mod", ignore_missing_data => 1);
+
+code::append_verbatim_code(model => $model, code_record => 'pk', pos => 'FIRST', code => [ 'X = X + 1' ]);
+my $record = $model->problems->[0]->pks->[0];
+my $code = $record->_format_record();
+is ($code->[0], '$PK "FIRST', "append_verbatim_code line 1");
+is ($code->[1], '"X = X + 1', "append_verbatim_code line 2");
+
+# generate_sum
 is_deeply (code::generate_sum(name => "ESUM1", terms => [ "ETA1", "ETA2", "ETA3" ]), "ESUM1 = ETA1 + ETA2 + ETA3", "generate_sum 1");
 is_deeply (code::generate_sum(name => "T", terms => [ "X" ]), "T = X", "generate_sum 2");
 
