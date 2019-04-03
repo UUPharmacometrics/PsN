50c0f307b5c24ab19bad94236671f0675008abc9
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2019-04-03 12:44:44 +0200

    R:qa: delete functions from PsN, all exist in PsNR

--- R-scripts/qa/get_ext_ofv.R
+++
@@ -1,7 +0,0 @@
-# get a ofv value from the ext file
-.get_ext_ofv <- function(ext_file,iteration=-1000000000){
-  read.table(ext_file,header=TRUE,skip=1,stringsAsFactors = F) %>%
-    dplyr::filter(ITERATION==iteration) %>%
-    dplyr::select(OBJ) %>% 
-    as.numeric()
-}
--- R-scripts/qa/get_rawres_ofv.R
+++
@@ -1,7 +0,0 @@
-# get a ofv value from the raw_results.csv
-.get_rawres_ofv <- function(rawres_file, row = 1){
-  read.csv(rawres_file) %>%
-    dplyr::slice(row) %>%
-    dplyr::select(ofv) %>% 
-    as.numeric()
-}
