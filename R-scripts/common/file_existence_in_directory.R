a5234b3dcff3dcc39e819222547ca57acb7ba57d
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2019-04-03 13:44:35 +0200

    R:common: Functions moved to the PsNR

--- R-scripts/common/file_existence_in_directory.R
+++
@@ -1,9 +0,0 @@
-file_existence_in_directory <- function(directory,file_name) {
-  all.files <- dir(path=directory)
-  if (any(all.files == file_name)) {
-    file_exists <- TRUE
-  } else {
-    file_exists <- FALSE
-  }
-  return(file_exists)
-}
