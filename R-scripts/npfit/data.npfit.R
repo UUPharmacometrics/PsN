2f20055fdc96ea95a52e33876c83547e2abf7fba
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2019-04-03 11:10:05 +0200

    R:npfit: Move functions to the PsNR package

--- R-scripts/npfit/data.npfit.R
+++
@@ -1,9 +0,0 @@
-data.npfit <- function(raw.nonparametric.file) {
-
-  raw_nonparametric_real <- read.csv(raw.nonparametric.file)
-  # delete rows with NA values
-  raw_nonparametric <- raw_nonparametric_real[complete.cases(raw_nonparametric_real),]
-  rownames(raw_nonparametric)=NULL
-  
-  return(raw_nonparametric)
-}
