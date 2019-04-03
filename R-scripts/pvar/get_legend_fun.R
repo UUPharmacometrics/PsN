65ce8346d40ebc68181305c70cd1395d2e539746
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2019-04-03 12:30:53 +0200

    R:pvar: Move functions to PsNR package

--- R-scripts/pvar/get_legend_fun.R
+++
@@ -1,9 +0,0 @@
-# function for getting legend from the plot
-get_legend <- function(myggplot){
-  pdf(NULL) # Workaround for bug in ggplot_gtable causing empty Rplots.pdf to be created
-  tmp <- ggplot_gtable(ggplot_build(myggplot))
-  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-  legend <- tmp$grobs[[leg]]
-  graphics.off()
-  return(legend)
-}
