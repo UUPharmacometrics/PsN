12ec3b9b2936a92cdae8ea528106fcec73b9eb3b
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2023-11-24 14:19:28 +0100

    Add random_get_seed to random module

--- lib/random.pm
+++ lib/random.pm
@@ -1,12 +1,15 @@
 package random;
 
 use strict;
+use Digest::SHA qw(sha1_hex);
 use Exporter qw(import);
 use PsN;
 
-our @EXPORT = qw(random_set_seed_from_phrase random_uniform_integer random_uniform random_permuted_index);
+our @EXPORT = qw(random_set_seed_from_phrase random_uniform_integer random_uniform random_permuted_index random_get_seed random_set_seed);
 
 our $old;
+our @global_seed;
+
 
 if (PsN::use_old_math_random) {
     require Math::Random;
@@ -22,11 +25,23 @@ if (PsN::use_old_math_random) {
 sub random_set_seed
 {
     if ($old) {
-        Math::Random::set_seed(@_);
+        Math::Random::random_set_seed(@_);
     } else {
         my ($s1, $s2) = @_; 
         my $seed = $s1 ^ $s2;
+        $global_seed[0] = $seed;
+        $global_seed[1] = 0;
         srand $seed;
+    }
+}
+
+
+sub random_get_seed
+{
+    if ($old) {
+        return Math::Random::random_get_seed();
+    } else {
+        return @global_seed;
     }
 }
 
@@ -37,9 +52,15 @@ sub random_set_seed_from_phrase
         Math::Random::random_set_seed_from_phrase(@_); 
         Math::Random::Free::random_set_seed_from_phrase(@_);    # FIXME: Remove this
     } else {
-        Math::Random::Free::random_set_seed_from_phrase(@_);
+        #Math::Random::Free::random_set_seed_from_phrase(@_);
         Math::Random::random_set_seed_from_phrase(@_);      # FIXME: Remove this
     }
+    my( $seed ) = @_;
+    # On 64-bit machine the max. value for srand() seems to be 2**50-1
+    my $value = hex substr( sha1_hex( $seed ), 0, 6 );
+    $global_seed[0] = $value;
+    $global_seed[1] = 0;
+    srand $value;
 }
 
 
